0c686fa0bfe61faed2af58e76a371029476fdda4
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-07-18 15:08:33 +0200

    multiple choice component example

---
+++ app/src/components/MultipleChoice.component.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card>
+    <v-container>
+      <h2>Multiple Choice Test</h2>
+
+      <p>{{ props.question }}</p>
+      <p v-if="submitted">Lösung:</p>
+
+      <v-checkbox v-for="(answer, index) in props.answers"
+                  :key="index"
+                  :label="answer.text"
+                  v-model="selectedAnswers[index]"
+                  :class="{ 'right': submitted && selectedAnswers[index] == answer.right,
+                'disabled': submitted,
+                'wrong': submitted && selectedAnswers[index] != answer.right}">
+      </v-checkbox>
+
+      <v-btn @click="submitAnswers">Submit</v-btn>
+    </v-container>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+
+import {onMounted, ref} from "vue";
+
+interface Answer {
+  text: string,
+  right: boolean,
+  id: number
+}
+
+interface Props {
+  question: string;
+  answers: Answer[];
+}
+
+const props = defineProps<Props>();
+const selectedAnswers = ref([]);
+const submitted = ref(false);
+
+function submitAnswers(): void {
+  submitted.value = true;
+}
+
+onMounted(() => {
+  selectedAnswers.value = props.answers.map(() => false);
+});
+
+</script>
+
+<style scoped>
+.right {
+  color: #52ff52;
+  font-weight: bold;
+}
+
+.wrong {
+  color: #ff4b4b;
+  font-weight: bold;
+}
+
+.disabled {
+  cursor: not-allowed;
+  pointer-events: none;
+}
+</style>
--- app/src/views/lesson/Lektionen.view.vue
+++ app/src/views/lesson/Lektionen.view.vue
@@ -1,8 +1,22 @@
 <template>
     <h1>Lektionen</h1>
+  <div>
+    <MultipleChoice :question=question :answers=answers
+    ></MultipleChoice>
+  </div>
 </template>
   
 <script setup lang="ts">
+
+import MultipleChoice from "@/components/MultipleChoice.component.vue";
+
+const question = "Welche dieser Aussagen stimmen?"
+
+const answers = [
+  { text: "Option 1", right: false, id: 1 },
+  { text: "Option 2", right: true, id: 2 },
+  { text: "Option 3", right: false, id: 3 },
+]
 
 </script>
   
