14f6253670750d990c3c8ccfaa401e73de5abbc8
Fabian Teufel <fabianteufel@me.com>
2023-07-19 17:38:40 +0200

    Revert "Auto import for components" This reverts commit ebbcbed6

--- app/src/components/Footer.component.vue
+++ app/src/components/Footer.component.vue
@@ -3,3 +3,4 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure unde nam eius iste rerum, nostrum aperiam debitis, possimus optio dignissimos sunt tempora? Omnis sed suscipit rem nam repudiandae facilis quod!</p>
   </v-footer>
 </template>
+
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,10 @@
 // Plugins
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import components from 'unplugin-vue-components/vite'
-import autoImport from 'unplugin-auto-import/vite'
 
 // Utilities
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'path';
-import { existsSync } from 'node:fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,23 +15,9 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
-    }),
-    autoImport({
-      dts: true,
-      imports:
-          ['vue', 'vue-router', 'pinia',
-            //{ from: 'vue-router', imports: ['Router, RouteLocationRaw'], type: true },
-            //{ from: 'vue-router', imports: ['createRouter', 'createWebHistory'] }
-          ],
-    }),
-    components({
-      dirs: [],
-      resolvers: [
-        (name) => {
-          const path = resolve(__dirname, './src/components', `${name}.component.vue`)
-          return existsSync(path) ? path : undefined
-        }
-      ],
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
     }),
   ],
   define: { 'process.env': {} },
