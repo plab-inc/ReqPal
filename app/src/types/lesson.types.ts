29560cf625cdc63843ad87ff83cff14c8391c181
magichoco <seifert.laura@gmx.de>
2023-08-02 10:43:07 +0200

    lesson types refactor

---
+++ app/src/types/lesson.types.ts
@@ -0,0 +1,30 @@
+export interface Lesson {
+    id: string;
+    title: string;
+    description: string;
+}
+
+export interface Question {
+    id: string;
+    lessonId: string;
+    type: string;
+    description: string;
+    answers: Answer[];
+    userResults: Result | null;
+}
+
+export interface Answer {
+    id: string,
+    description: string;
+    solution: boolean;
+}
+
+export interface Result {
+    wholeAnswerIsCorrect: boolean;
+    results: answerResults[];
+}
+
+export interface answerResults {
+    id: string,
+    answerIsCorrect: boolean;
+}
