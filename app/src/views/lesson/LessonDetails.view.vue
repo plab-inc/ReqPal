995069416ec5e0cc54dfbf2d07ca22c6b043fa89
magichoco <seifert.laura@gmx.de>
2023-08-02 12:13:06 +0200

    show data only when finished loading

--- app/src/views/lesson/LessonDetails.view.vue
+++ app/src/views/lesson/LessonDetails.view.vue
@@ -1,20 +1,19 @@
 <template>
 
-  <div v-if="!currentLesson">
+  <div v-if="!dataLoaded">
     <p>Loading Lesson...</p>
   </div>
 
   <div v-else>
-    <h1>{{ currentLesson.title }}</h1>
+    <h1>{{ currentLesson?.title }}</h1>
   </div>
 
-  <div v-if="currentQuestions.length <= 0">
+  <div v-if="dataLoaded && currentQuestions.length <= 0">
     <p>No questions yet!</p>
   </div>
 
-  <div v-else>
-    <MultipleChoice v-for="question in currentQuestions" :question=question
-    ></MultipleChoice>
+  <div v-if="dataLoaded && currentQuestions.length > 0">
+    <MultipleChoice v-for="question in currentQuestions" :key="question.id" :question="question"></MultipleChoice>
   </div>
 
 </template>
@@ -22,20 +21,23 @@
 <script setup lang="ts">
 
 import MultipleChoice from "@/components/MultipleChoice.component.vue";
-import {useLessonStore} from "@/stores/lesson.store";
-import {useRoute, useRouter} from "vue-router";
-import {storeToRefs} from 'pinia'
+import { useLessonStore } from "@/stores/lesson.store";
+import { useRoute } from "vue-router";
+import { storeToRefs } from 'pinia'
 
-const router = useRouter();
 const route = useRoute();
 
 const lessonId = route.params.lessonId.toString();
 const lessonStore = useLessonStore();
 
-const {currentLesson, currentQuestions} = storeToRefs(lessonStore)
+const { currentLesson, currentQuestions } = storeToRefs(lessonStore)
 
-lessonStore.fetchLessonById(lessonId);
-lessonStore.fetchQuestionsForLesson(lessonId);
+const dataLoaded = ref(false);
+
+onBeforeMount(async () => {
+  await lessonStore.fetchLessonById(lessonId);
+  await lessonStore.fetchQuestionsForLesson(lessonId);
+  dataLoaded.value = true;
+})
 
 </script>
-  
