c38e83633613fe9054ba592d4fb1e9e9e6f24a3e
Laura <seifert.laura@gmx.de>
2023-08-14 17:49:57 +0200

    add user choices and solutions + feedback to component

--- app/src/components/TrueOrFalse.component.vue
+++ app/src/components/TrueOrFalse.component.vue
@@ -1,10 +1,18 @@
 <template>
   <v-card>
     <v-container>
-      <h2>True Or False Test</h2>
+      <h2>True Or False?</h2>
 
       <p>{{ props.question.description }}</p>
-      <p v-if="submitted">Lösung:</p>
+      <p v-if="submitted">Solution:</p>
+
+      <v-radio-group v-model="selectedAnswer">
+        <v-radio label="True" value="true"
+                 :class="{'disabled': submitted,'right': result === true, 'wrong': result === false}"></v-radio>
+        <v-radio label="False" value="false"
+                 :class="{'disabled': submitted, 'right': result === false, 'wrong': result === true}"></v-radio>
+        <p v-if="triedToSubmit">Please choose an answer before submitting!</p>
+      </v-radio-group>
 
       <v-btn @click="submitAnswers">Submit</v-btn>
 
@@ -16,24 +24,29 @@
 
 import {ref} from "vue";
 import {useLessonStore} from "@/stores/lesson.store";
-import {Answer, Question} from "@/types/lesson.types";
+import {Question} from "@/types/lesson.types";
 
 interface Props {
   question: Question;
 }
 
 const props = defineProps<Props>();
-const selectedAnswers = ref<boolean[]>([]);
+const selectedAnswer = ref<boolean>();
 const submitted = ref(false);
+const triedToSubmit = ref(false);
 const lessonStore = useLessonStore();
+let result: boolean | null;
 
 async function submitAnswers(): Promise<void> {
+  if (!selectedAnswer.value) {
+    triedToSubmit.value = true;
+    return;
+  }
 
+  result = await lessonStore.fetchTrueFalseAnswersForQuestion(props.question.id);
+  submitted.value = true;
+  triedToSubmit.value = false;
 }
-
-onBeforeMount(async () => {
-
-});
 
 </script>
 
