4d68d9c84eba04de16b993129d302bce55eb3d4f
Laura <seifert.laura@gmx.de>
2023-08-14 14:03:04 +0200

    use types for questions, basic true or false component

--- app/src/views/lesson/LessonDetails.view.vue
+++ app/src/views/lesson/LessonDetails.view.vue
@@ -9,7 +9,8 @@
   </div>
 
   <div v-if="currentQuestions.length > 0">
-    <MultipleChoice v-for="question in currentQuestions" :key="question.id" :question="question"></MultipleChoice>
+    <MultipleChoice v-for="question in multipleChoiceQuestions" :key="question.id" :question="question"></MultipleChoice>
+    <TrueOrFalse v-for="question in trueOrFalseQuestions" :key="question.id" :question="question"></TrueOrFalse>
   </div>
 
 </template>
@@ -17,10 +18,12 @@
 <script setup lang="ts">
 
 import MultipleChoice from "@/components/MultipleChoice.component.vue";
+import TrueOrFalse from "@/components/TrueOrFalse.component.vue";
 import {useLessonStore} from "@/stores/lesson.store";
 import {storeToRefs} from 'pinia'
 
 const lessonStore = useLessonStore();
 const {currentLesson, currentQuestions} = storeToRefs(lessonStore)
-
+const multipleChoiceQuestions = computed(() => lessonStore.getMultipleChoiceQuestions);
+const trueOrFalseQuestions = computed(() => lessonStore.getTrueOrFalseQuestions);
 </script>
