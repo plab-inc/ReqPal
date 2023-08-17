464e657dc2d5ee967f1e54aed782b33436d7e8e6
Laura <seifert.laura@gmx.de>
2023-08-17 16:13:56 +0200

    changed the color of vtabs in navigation

--- app/src/components/Navigation.component.vue
+++ app/src/components/Navigation.component.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app-bar color="surface" class="px-3" flat>
-    <v-app-bar-title @click="$router.push('/')"> Lernplattform </v-app-bar-title>
+    <v-app-bar-title @click="$router.push('/')"> Lernplattform</v-app-bar-title>
 
-
-    <v-tabs centered color="surface">
-      <v-tab variant="plain" v-for="(link, index) in links" :key="index" :to="{ name: link.name}">
+    <v-tabs centered>
+      <v-tab v-for="(link, index) in links" :key="index" :to="{ name: link.name}">
         {{ link.title }}
       </v-tab>
     </v-tabs>
@@ -13,14 +12,14 @@
     <div v-if="authStore.session">
       <span @click="$router.push('/profile')">{{ profileStore.username }}</span>
     </div>
-    <v-btn v-if="authStore.session" variant="outlined" @click="logout()"> Logout </v-btn>
-    <v-btn v-if="!authStore.session" variant="outlined" @click="$router.push('login')"> Login </v-btn>
+    <v-btn v-if="authStore.session" variant="outlined" @click="logout()"> Logout</v-btn>
+    <v-btn v-if="!authStore.session" variant="outlined" @click="$router.push('login')"> Login</v-btn>
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
-import { useAuthStore } from "@/stores/auth.store";
-import { useProfileStore } from "@/stores/profile.store";
+import {useAuthStore} from "@/stores/auth.store";
+import {useProfileStore} from "@/stores/profile.store";
 
 import router from "@/router";
 
@@ -30,17 +29,17 @@ interface Link {
 }
 
 const links: Link[] = [
-  { title: "Dashboard", name: "Dashboard" },
-  { title: "Lektionen", name: "AllLessons" },
-  { title: "Szenario", name: "Szenario" },
-  { title: "Feedback", name: "Feedback" }
+  {title: "Dashboard", name: "Dashboard"},
+  {title: "Lektionen", name: "AllLessons"},
+  {title: "Szenario", name: "Szenario"},
+  {title: "Feedback", name: "Feedback"}
 ];
 
 const authStore = useAuthStore();
 const profileStore = useProfileStore();
 
 onMounted(() => {
-  if(authStore.session && authStore.user) {
+  if (authStore.session && authStore.user) {
     profileStore.fetchProfile(authStore.user?.id);
   }
 });
