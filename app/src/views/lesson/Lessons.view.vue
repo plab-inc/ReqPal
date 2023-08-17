202d77cfaf08bf374dca53bbdad2469cfa8deffe
Laura <seifert.laura@gmx.de>
2023-08-17 16:14:59 +0200

    reformat, small layout changes

--- app/src/views/lesson/Lessons.view.vue
+++ app/src/views/lesson/Lessons.view.vue
@@ -1,23 +1,24 @@
 <template>
-  <h1>Alle Lektionen</h1>
+  <v-container>
+    <h1>Alle Lektionen</h1>
 
-  <div>
-    <div v-if="!lessonStore.lessons.length">
-      <p>Looking for lessons...</p>
+    <div>
+      <div v-if="!lessonStore.lessons.length">
+        <p>Looking for lessons...</p>
+      </div>
+
+      <div v-else>
+        <ul>
+          <li v-for="lesson in lessons">
+            <router-link :to="{ name: 'LessonDetails', params: { lessonId: lesson.id } }">
+              {{ lesson.title }}
+              {{ lesson.description }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
-
-    <div v-else>
-      <ul>
-        <li v-for="lesson in lessons">
-          <router-link :to="{ name: 'LessonDetails', params: { lessonId: lesson.id } }">
-            {{ lesson.title }}
-            {{ lesson.description }}
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </div>
-
+  </v-container>
 </template>
 
 <script setup lang="ts">
