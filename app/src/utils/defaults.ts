649db32921a311ed7eeb1e8077cbe5fdf7229886
Laura <seifert.laura@gmx.de>
2023-08-17 16:13:56 +0200

    changed the color of vtabs in navigation

--- app/src/utils/defaults.ts
+++ app/src/utils/defaults.ts
@@ -1,38 +1,29 @@
-import { DefaultsInstance } from "vuetify/lib/framework.mjs";
-import { colors } from "@/utils/colors";
+import {DefaultsInstance} from "vuetify/lib/framework.mjs";
+import {colors} from "@/utils/colors";
 
 export const defaults: DefaultsInstance = {
-  VAppBar: {
-    elevation: 3,
-    color: "primary",
-  },
-  VTabs: {
-    color: "surface",
-  },
-  VTab:{
-    elevation: 0,
-    ripple: false,
-    variant: "plain",
-  },
-  /*
-  VBtn: {
-    variant: "flat",
-    height: 50,
-    rounded: "lg",
-    size: "small",
-    color: "primary"
-  },
-  */
-  VTextField: {
-    color: "primary",
-    variant: "solo-filled",
-    density: "comfortable",
-  },
-  VAvatar: {
-    color: colors.slate[900],
-    size: "32"
-  },
-  VCol: {
-    cols: "12"
-  }
+    VAppBar: {
+        elevation: 3,
+        color: "primary",
+    },
+    VTabs: {
+        color: colors.sky["300"]
+    },
+    VTab: {
+        elevation: 0,
+        ripple: false,
+        variant: "plain",
+    },
+    VTextField: {
+        color: "primary",
+        variant: "solo-filled",
+        density: "comfortable",
+    },
+    VAvatar: {
+        color: colors.slate[900],
+        size: "32"
+    },
+    VCol: {
+        cols: "12"
+    }
 };
