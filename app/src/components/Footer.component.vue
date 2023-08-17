2d757693c7e759e893343a6578905f4064e075b0
Laura <seifert.laura@gmx.de>
2023-08-17 16:14:59 +0200

    reformat, small layout changes

--- app/src/components/Footer.component.vue
+++ app/src/components/Footer.component.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-footer> 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure unde nam eius iste rerum, nostrum aperiam debitis, possimus optio dignissimos sunt tempora? Omnis sed suscipit rem nam repudiandae facilis quod!</p>
+  <v-footer>
+    <v-container>
+      <v-row my="16">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure unde nam eius iste rerum, nostrum aperiam debitis, possimus optio dignissimos sunt tempora? Omnis sed suscipit rem nam repudiandae facilis quod!</p>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
 
+<script setup lang="ts">
+</script>
