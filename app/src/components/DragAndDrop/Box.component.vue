f469ed5c8bd3e01bc80aad591c8a03b26bb9241e
Fabian Teufel <fabianteufel@me.com>
2023-08-20 16:23:31 +0200

    vue3-dnd integration and example

---
+++ app/src/components/DragAndDrop/Box.component.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { useDrag } from 'vue3-dnd'
+import { ItemTypes } from '@/types/DragAndDrop.types'
+import { toRefs } from '@vueuse/core'
+
+const props = defineProps<{
+  id: any
+  left: number
+  top: number
+  hideSourceOnDrag?: boolean
+}>()
+
+const [collect, drag] = useDrag(() => ({
+  type: ItemTypes.BOX,
+  item: { id: props.id, left: props.left, top: props.top },
+  collect: monitor => ({
+    isDragging: monitor.isDragging(),
+  }),
+}))
+const { isDragging } = toRefs(collect)
+</script>
+
+<template>
+  <div v-if="isDragging && hideSourceOnDrag" :ref="drag" />
+  <div
+      v-else
+      :ref="drag"
+      class="box"
+      :style="{ left: `${left}px`, top: `${top}px` }"
+      role="Box"
+      data-testid="box"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  position: absolute;
+  padding: 0.5rem 1rem;
+  border: 1px dashed gray;
+  border-radius: 5px;
+  cursor: move;
+}
+</style>
