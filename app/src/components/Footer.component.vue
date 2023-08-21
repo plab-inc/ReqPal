fbe6dee179afa9440289f56ce6363ff91a5514ee
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-08-21 16:34:43 +0200

    update footer layout

--- app/src/components/Footer.component.vue
+++ app/src/components/Footer.component.vue
@@ -1,12 +1,30 @@
 <template>
   <v-footer>
     <v-container>
-      <v-row my="16">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure unde nam eius iste rerum, nostrum aperiam debitis, possimus optio dignissimos sunt tempora? Omnis sed suscipit rem nam repudiandae facilis quod!</p>
+      <v-row>
+        <v-col sm="6" md="2">
+          <div class="text-body-1">
+            <RouterLink :to="{name: 'Home'}" class="text-body-1 text-decoration-none text-primary">Lernplattform
+            </RouterLink>
+            <p>&copy; Projektarbeit 2023</p>
+          </div>
+        </v-col>
+        <v-col sm="6" md="10">
+          <p class="text-body-1">Menu</p>
+          <v-list>
+            <v-list-item v-for="(link, index) in links" :key="index">
+              <RouterLink :to="{ name: link.name}" class="text-body-1 text-decoration-none text-primary">{{
+                  link.title
+                }}
+              </RouterLink>
+            </v-list-item>
+          </v-list>
+        </v-col>
       </v-row>
     </v-container>
   </v-footer>
 </template>
 
 <script setup lang="ts">
+import {links} from "@/types/navigationLinks.types";
 </script>
