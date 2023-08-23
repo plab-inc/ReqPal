7787a4fd6c77353de59f03b66be6944e01ed1e8c
Fabian Teufel <fabianteufel@me.com>
2023-08-23 23:43:25 +0200

    Improved drag/drop logic+layout

--- app/src/components/DragAndDrop/DragAndDrop.component.vue
+++ app/src/components/DragAndDrop/DragAndDrop.component.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import Container from "@/components/DragAndDrop/Container.component.vue";
+import CustomDragLayer from "@/components/DragAndDrop/CustomDragLayer.component.vue";
 import { useContainerStore } from "@/stores/DragAndDrop/container.store";
 
 const boxStore = useContainerStore();
@@ -11,9 +12,12 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  dropFields: () => ['TestA', 'TestB', 'TestC'],
-  answers: () => ['TestA', 'TestB'],
+  dropFields: () => ['DropFieldA', 'DropFieldB', 'DropFieldC'],
+  answers: () => ['AnswerA', 'AnswerB', "AnswerC"],
 });
+
+const firstContainerHeight = ref(400);
+const otherContainerHeight = computed(() => firstContainerHeight.value / 2);
 
 </script>
 
@@ -21,17 +25,28 @@ const props = withDefaults(defineProps<Props>(), {
 
   <v-container>
     <v-card>
-      <v-container ref="containerRef">
-
-        <Container :box-titles="answers" container-id="Answers" class="above-fields"/>
-
-        <div class="drop-fields">
-          <Container
-              v-for="(field) in dropFields"
-              :container-id="field"
+      <v-container>
+        <v-row no-gutters>
+          <v-col>
+            <Container :box-titles="answers" container-id="Answers" title="Antworten" :style="{ height: `${firstContainerHeight}px` }"/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+              v-for="(field, index) in dropFields"
+              :key="index"
+              cols="12" sm="6" md="4"
               class="drop-field"
-          />
-        </div>
+          >
+            <Container :container-id="field" :title="field" :style="{ height: `${otherContainerHeight}px` }"/>
+          </v-col>
+        </v-row>
+        <CustomDragLayer/>
+        <v-row align="center">
+          <v-col>
+            <v-btn color="primary" @click="console.log(boxStore.getAnswersInContainers())" block>Print to Console</v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </v-card>
   </v-container>
@@ -40,22 +55,4 @@ const props = withDefaults(defineProps<Props>(), {
 
 <style scoped>
 
-.above-fields {
-  width: 100%;
-  text-align: center;
-  height: 300px;
-  border: 1px solid #ccc;
-  margin-bottom: 10px;
-}
-
-.drop-fields {
-  display: flex;
-}
-
-.drop-field {
-  flex: 1;
-  padding: 50px;
-  border: 1px solid #ccc;
-  text-align: center;
-}
 </style>
