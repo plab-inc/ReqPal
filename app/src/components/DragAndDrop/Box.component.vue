c121fe8a6f1a57a2e94e9d553c9c45156eb2289b
Fabian Teufel <fabianteufel@me.com>
2023-08-23 23:43:25 +0200

    Improved drag/drop logic+layout

--- app/src/components/DragAndDrop/Box.component.vue
+++ app/src/components/DragAndDrop/Box.component.vue
@@ -1,47 +1,33 @@
 <script lang="ts" setup>
-import { useDrag } from 'vue3-dnd'
-import { ItemTypes } from '@/types/dragAndDrop.types'
-import { toRefs } from '@vueuse/core'
 
 const props = defineProps<{
-  id: string;
-  left: number;
-  top: number;
-  containerId: string;
+  title: string
+  preview?: boolean
 }>();
 
-const [collect, drag] = useDrag(() => ({
-  type: ItemTypes.BOX,
-  item: { id: props.id, left: props.left, top: props.top, containerId: props.containerId },
-  collect: monitor => ({
-    isDragging: monitor.isDragging(),
-  }),
-}))
 
-const { isDragging } = toRefs(collect)
 </script>
 
 <template>
-  <div v-if="isDragging" :ref="drag"/>
-  <div v-else
-       :ref="drag"
-       class="box"
-       :style="{
-        left: `${props.left}px`,
-        top: `${props.top}px`,
-      }"
+  <div
+      class="box"
+      :role="preview ? 'BoxPreview' : 'Box'"
   >
-    <slot></slot>
+    {{ title }}
   </div>
 </template>
 
 <style scoped>
 .box {
-  position: absolute;
+  width: 150px;
+  font-size: 0.875em;
   padding: 0.5rem 1rem;
-  border: 1px dashed gray;
+  border: 1px solid lightgray;
+  background-color: rgb(var(--v-theme-info));
   border-radius: 5px;
   cursor: move;
   user-select: none;
+  text-align: center;
+  height: auto;
 }
 </style>
