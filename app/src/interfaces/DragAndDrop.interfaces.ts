8b9667153a723da84143483778ee5e8730de8099
Fabian Teufel <fabianteufel@me.com>
2023-08-23 20:11:49 +0200

    Overworked store and box generation

--- app/src/interfaces/DragAndDrop.interfaces.ts
+++ app/src/interfaces/DragAndDrop.interfaces.ts
@@ -1,12 +1,7 @@
 export interface DragItem {
-    type: string
     id: string
     top: number
     left: number
     containerId: string
-}
-interface Box {
-    top: number;
-    left: number;
-    title: string;
+    title: string
 }
