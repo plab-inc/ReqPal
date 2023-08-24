4f6465b151a9d715a974f4460bf9ef7abf969232
Fabian Teufel <fabianteufel@me.com>
2023-08-24 10:24:05 +0200

    Remove DnD Test Component from home

--- app/src/views/home/Home.view.vue
+++ app/src/views/home/Home.view.vue
@@ -1,17 +1,7 @@
 <template>
   <h1>Willkommen auf der Lernplattform!</h1>
-
-  <DragAndDrop/>
-
-
-
-
-
 </template>
 
 <script setup lang="ts">
-
-import DragAndDrop from "@/components/DragAndDrop/DragAndDrop.component.vue";
-import Upload from "@/components/Upload.component.vue";
 
 </script>
