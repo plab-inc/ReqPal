8d42115798026670d783fbc3d2c5be6c092dcbe4
Laura <seifert.laura@gmx.de>
2023-08-25 17:30:26 +0200

    clean up

--- app/src/components/Sortable/Sortable.component.vue
+++ app/src/components/Sortable/Sortable.component.vue
@@ -2,9 +2,7 @@
 import {HTML5Backend} from "react-dnd-html5-backend";
 import Container from "@/components/Sortable/Container.component.vue";
 import {DndProvider} from "vue3-dnd";
-import {Answer, Question} from "@/types/lesson.types";
-import {ref} from "vue";
-import {useLessonStore} from "@/stores/lesson.store";
+import {Question} from "@/types/lesson.types";
 
 interface Props {
   question: Question;
@@ -23,7 +21,7 @@ const answers = [{id: 1, text: 'Aussage 1'}, {id: 2, text: 'Aussage 2'}, {id: 3,
     <v-container>
       <v-row>
         <v-col order="1" order-md="2" md="6" align-self="center" class="text-center">
-          <h3>{{question.description}}</h3>
+          <h3>{{ question.description }}</h3>
         </v-col>
         <v-col order="2" order-md="1" md="6">
           <DndProvider :backend="HTML5Backend">
