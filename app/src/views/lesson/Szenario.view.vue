950cda4792bfc8877009e9ca8ad466624311e980
Laura <seifert.laura@gmx.de>
2023-08-25 17:30:26 +0200

    clean up

--- app/src/views/lesson/Szenario.view.vue
+++ app/src/views/lesson/Szenario.view.vue
@@ -2,12 +2,9 @@
   <v-container>
     <h1>Szenario</h1>
     <p>Szenario Auswahl</p>
-    <Sortable></Sortable>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import Sortable from "@/components/Sortable/Sortable.component.vue";
-
 </script>
   
