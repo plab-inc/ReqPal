eaee7ac3cc778e3aea8d54ef0bf462ed4a50f715
magichoco <seifert.laura@gmx.de>
2023-08-31 20:03:54 +0200

    save created question in database and add to currentQuestions in store

--- app/src/components/TrueOrFalse/TrueOrFalseForm.component.vue
+++ app/src/components/TrueOrFalse/TrueOrFalseForm.component.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
 
 import {useLessonStore} from "@/stores/lesson.store";
-import {requiredRule} from "@/utils/validationRules";
+import {booleanValueRule, requiredRule} from "@/utils/validationRules";
+import {addErrorAlert, addSuccessAlert} from "@/services/alert.service";
+import router from "@/router";
 
 const lessonStore = useLessonStore();
 const lesson = lessonStore.getLessonById;
-const solution = ref<boolean>();
+const solution = ref<boolean>(true);
 const question = ref("");
 const isFormValid = ref(false);
 const rules = {
   required: requiredRule,
+  requiredBool: booleanValueRule,
 };
 
 async function submitQuestion(): Promise<void> {
-  console.log("Submitted:")
-  console.log("Question: " + question.value)
-  console.log("Solution: " + solution.value)
+  try {
+    if (lesson) {
+      await lessonStore.addTrueOrFalseQuestion(lesson.id, question.value, solution.value);
+      addSuccessAlert("Question added to lesson " + lesson.id + ": " + lesson.title);
+      await router.push({name: "AllLessons"})
+    } else {
+      addErrorAlert("Lesson not found.");
+    }
+  } catch (error: any) {
+    addErrorAlert("Failed to create question: " + error.message);
+  }
 }
 </script>
 
@@ -30,16 +41,13 @@ async function submitQuestion(): Promise<void> {
         :rules="[rules.required]"
     ></v-text-field>
 
-    <v-radio-group v-model="solution" :rules="[rules.required]" label="Solution of the question:">
-      <v-radio label="True" value="true"></v-radio>
-      <v-radio label="False" value="false"></v-radio>
+    <v-radio-group v-model="solution" :rules="[rules.requiredBool]" label="Solution of the question:">
+      <v-radio label="True" v-bind:value="true"></v-radio>
+      <v-radio label="False" v-bind:value="false"></v-radio>
     </v-radio-group>
 
-    <v-btn block type="submit" :disabled="!isFormValid" @click="submitQuestion">Add question</v-btn>
+    <v-btn block type="submit" :disabled="!isFormValid">Add question</v-btn>
   </v-form>
-
-  <h4>{{ question }}</h4>
-  <h4>{{ solution }}</h4>
 </template>
 
 <style scoped>
