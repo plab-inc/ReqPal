66eebcdbcff2b2cdb554c52cbbee661267fdb5d7
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 20:11:53 +0200

    changed profile layout

--- app/src/components/Profile/UserData/UserDetails.component.vue
+++ app/src/components/Profile/UserData/UserDetails.component.vue
@@ -1,20 +1,19 @@
 <template>
   <v-row>
-    <v-col order="2" order-lg="1" lg="6"
-           class="d-flex flex-column align-center justify-center align-lg-start justify-lg-center">
+    <v-col class="d-flex flex-column align-center justify-center">
+      <v-avatar
+          color="secondary"
+          image="@/assets/images/profile-test.jpg"
+          alt="profilePicture"
+          size="160"></v-avatar>
+    </v-col>
+    <v-col class="d-flex flex-column align-center justify-center">
       <h1 class="profileData">{{ user?.user_metadata.username }}</h1>
       <h2 class="profileData">{{ user?.email }}</h2>
       <h2 class="profileData">Joined {{ joinedDate }}</h2>
     </v-col>
-    <v-col order="1" order-lg="2" lg="6" class="d-flex align-center justify-center align-lg-center justify-lg-end">
-      <div class="d-flex flex-column align-center justify-center">
-        <v-avatar class="profileData"
-                  color="secondary"
-                  image="@/assets/images/profile-test.jpg"
-                  alt="profilePicture"
-                  size="160"></v-avatar>
-        <v-btn class="profileData">Edit Profile</v-btn>
-      </div>
+    <v-col class="d-flex justify-end">
+      <v-btn class="profileData">Edit Profile</v-btn>
     </v-col>
   </v-row>
 </template>
