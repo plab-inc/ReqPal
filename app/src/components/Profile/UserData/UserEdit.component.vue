1065044967b89b5df2ce7286b5ec508a83083f76
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 18:01:06 +0200

    fix warnings

--- app/src/components/Profile/UserData/UserEdit.component.vue
+++ app/src/components/Profile/UserData/UserEdit.component.vue
@@ -13,25 +13,4 @@ const user: User | null = authStore.user;
 
 </script>
 <style scoped>
-
-.profileContainerCol {
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.profileContainerRow {
-  border: 1px solid black;
-  padding: 10px 20px 10px 20px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.profileData {
-  margin: 5px 0px 5px 0px;
-}
-
 </style>
