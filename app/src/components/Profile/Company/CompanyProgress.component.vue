7316e760f640e174247f891076f51ba0df1ddc42
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 20:10:35 +0200

    add company profile components

---
+++ app/src/components/Profile/Company/CompanyProgress.component.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-col class="d-flex align-center justify-space-between flex-wrap">
+      <h1>Company Progress</h1>
+      <v-btn variant="plain">Details</v-btn>
+    </v-col>
+    <v-col>
+      <h2>Level 6</h2>
+      <v-progress-linear color="primary" model-value="50" :height="12"></v-progress-linear>
+    </v-col>
+    <v-col>
+      <h2>Zufriedenheit</h2>
+      <v-progress-linear color="primary" model-value="80" :height="12"></v-progress-linear>
+    </v-col>
+    <v-col>
+      <h2>Unzufriedenheit</h2>
+      <v-progress-linear color="primary" model-value="20" :height="12"></v-progress-linear>
+    </v-col>
+    <v-col>
+      <h2>Gewinne</h2>
+      <v-progress-linear color="primary" model-value="100" :height="12"></v-progress-linear>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
