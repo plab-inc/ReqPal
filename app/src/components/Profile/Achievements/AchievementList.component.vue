d36b785c43a727065c91172c82cec82b2ab7c694
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 19:03:20 +0200

    add achievements to profile

--- app/src/components/Profile/Achievements/AchievementList.component.vue
+++ app/src/components/Profile/Achievements/AchievementList.component.vue
@@ -1,29 +1,23 @@
 <template>
-
-    <div class="profileContainerRow">
+  <v-row>
+    <v-col class="d-flex align-center justify-space-between flex-wrap">
       <h1>Achievements</h1>
       <v-btn variant="plain">View All</v-btn>
-    </div>
-
+    </v-col>
+    <v-col class="d-flex flex-column align-center justify-center flex-wrap">
+      <AchievementItem
+          v-for="(item) in items"
+          :text="item">
+      </AchievementItem>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
-import {useAuthStore} from "@/stores/auth.store";
-import {User} from "@supabase/supabase-js";
+import AchievementItem from "@/components/Profile/Achievements/AchievementItem.component.vue";
 
-const authStore = useAuthStore();
-const user: User | null = authStore.user;
+const items = ['1000 Punkte', '20 Lektionen', '35 ohne Fehler']
 
 </script>
 <style scoped>
-
-.profileContainerRow {
-  border: 1px solid black;
-  padding: 10px 20px 10px 20px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-}
-
 </style>
