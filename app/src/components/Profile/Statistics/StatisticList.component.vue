cc16be93742ccbf628875c5f1ac5b32b48503f54
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 19:03:46 +0200

    update style and layout

--- app/src/components/Profile/Statistics/StatisticList.component.vue
+++ app/src/components/Profile/Statistics/StatisticList.component.vue
@@ -1,13 +1,15 @@
 <template>
   <v-row>
-    <v-col>
+    <v-col class="d-flex align-center justify-space-between flex-wrap">
       <h1>Statistics</h1>
+      <v-btn variant="plain">Details</v-btn>
     </v-col>
-    <v-col class="d-flex align-center justify-space-evenly flex-wrap">
+    <v-col
+        v-for="(item) in items"
+        sm="6"
+        class="d-flex align-center justify-space-evenly flex-wrap">
       <StatisticItem
-          v-for="(item, index) in items"
-          :key="index"
-          :highlight="index % 2 === 0"
+          class="flex-grow-1"
           :text="item">
       </StatisticItem>
     </v-col>
@@ -17,8 +19,7 @@
 <script setup lang="ts">
 import StatisticItem from "@/components/Profile/Statistics/StatisticItem.component.vue";
 
-const items = ['1000 Punkte', '20 Lektionen', '35 ohne Fehler', '20 Lektionen', '1000 Punkte',
-  '35 ohne Fehler', '20 Lektionen', '1000 Punkte']
+const items = ['Level 30', '1000 Punkte', '20 Lektionen', '35 ohne Fehler']
 
 </script>
 <style scoped>
