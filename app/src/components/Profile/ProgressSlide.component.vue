5bdc76ddbdd957fc94b3a0d174ae7bb11a46eebc
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 20:15:56 +0200

    changed folder and margins

--- app/src/components/Profile/ProgressSlide.component.vue
+++ app/src/components/Profile/ProgressSlide.component.vue
@@ -1,16 +1,16 @@
 <template>
   <v-window show-arrows>
     <v-window-item>
-      <UserProgress class="mx-16"></UserProgress>
+      <UserProgress class="mr-16"></UserProgress>
     </v-window-item>
     <v-window-item>
-      <CompanyDetails class="mx-16"></CompanyDetails>
+      <CompanyDetails class="ml-16"></CompanyDetails>
     </v-window-item>
   </v-window>
 </template>
 
 <script setup lang="ts">
-import UserProgress from "@/components/Profile/Statistics/UserProgress.component.vue";
+import UserProgress from "@/components/Profile/UserData/UserProgress.component.vue";
 import CompanyDetails from "@/components/Profile/Company/CompanyDetails.component.vue";
 </script>
 
