4628bca508d614a52dc15ef8a5e17d9631e4aed8
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 16:24:34 +0200

    added basic layout for profile view and profile components

---
+++ app/src/components/Profile/Profile.component.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container class="border">
+    <UserData></UserData>
+  </v-container>
+
+  <v-divider class="ma-4"></v-divider>
+
+  <v-container class="border">
+    <StatisticList></StatisticList>
+  </v-container>
+
+  <v-divider class="ma-4"></v-divider>
+
+  <v-container class="border">
+    <AchievementList></AchievementList>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import AchievementList from "@/components/Profile/Achievements/AchievementList.component.vue";
+import UserData from "@/components/Profile/UserData/UserDetails.component.vue";
+import StatisticList from "@/components/Profile/Statistics/StatisticList.component.vue";
+
+</script>
+<style scoped>
+
+.border {
+  border: 1px solid black
+}
+
+</style>
