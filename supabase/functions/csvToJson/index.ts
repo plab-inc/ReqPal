bd81785978fd3d132be3858dd7d9640126df4189
Fabian Teufel <fabianteufel@me.com>
2023-09-21 18:29:33 +0200

    Handle CORS Preflight requests

--- supabase/functions/csvToJson/index.ts
+++ supabase/functions/csvToJson/index.ts
@@ -18,7 +18,16 @@ interface RequirementsJSON {
     requirements: Requirement[];
 }
 
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
+
 serve(async (req)=>{
+
+    if (req.method === 'OPTIONS') {
+        return new Response('ok', { headers: corsHeaders })
+    }
     
     const contentType = req.headers.get('Content-Type');
     if (!contentType || !contentType.includes('multipart/form-data')) {
