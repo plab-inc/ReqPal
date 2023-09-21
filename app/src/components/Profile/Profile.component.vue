8a1b3f7c067b5c9143df8036435cfa0367a64879
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 19:04:06 +0200

    add progress component

--- app/src/components/Profile/Profile.component.vue
+++ app/src/components/Profile/Profile.component.vue
@@ -1,16 +1,22 @@
 <template>
 
   <v-container>
-    <UserData></UserData>
+    <UserDetails></UserDetails>
   </v-container>
 
-  <v-divider class="ma-4"></v-divider>
+  <v-divider class="mt-4"></v-divider>
+
+  <v-container>
+    <UserProgress></UserProgress>
+  </v-container>
+
+  <v-divider class="mt-4"></v-divider>
 
   <v-container>
     <StatisticList></StatisticList>
   </v-container>
 
-  <v-divider class="ma-4"></v-divider>
+  <v-divider class="mt-4"></v-divider>
 
   <v-container>
     <AchievementList></AchievementList>
@@ -19,8 +25,9 @@
 
 <script setup lang="ts">
 import AchievementList from "@/components/Profile/Achievements/AchievementList.component.vue";
-import UserData from "@/components/Profile/UserData/UserDetails.component.vue";
+import UserDetails from "@/components/Profile/UserData/UserDetails.component.vue";
 import StatisticList from "@/components/Profile/Statistics/StatisticList.component.vue";
+import UserProgress from "@/components/Profile/Statistics/Progress.component.vue";
 
 </script>
 <style scoped>
