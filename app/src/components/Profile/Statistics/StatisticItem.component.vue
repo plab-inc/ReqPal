e84d8011a04572f797a4d90ffc9f3e9073a3aefc
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-21 19:03:46 +0200

    update style and layout

--- app/src/components/Profile/Statistics/StatisticItem.component.vue
+++ app/src/components/Profile/Statistics/StatisticItem.component.vue
@@ -1,26 +1,23 @@
 <template>
-  <div :class="'bg-'+color" class="rounded-pill pa-3 ma-3 item">
-    <h1>{{ text }}</h1>
+  <div class="pa-4 ma-3 item bg-primary d-flex align-center justify-center">
+    <h1 class="text-center">{{ text }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 interface Props {
   text: String,
-  highlight: boolean
 }
 
 const props = defineProps<Props>();
-let color: String = 'info';
-if (props.highlight) {
-  color = 'primary';
-}
 
 </script>
 <style scoped>
 
 .item {
   border: 2px solid white;
+  border-radius: 40px;
+  height: 100%;
 }
 
 </style>
