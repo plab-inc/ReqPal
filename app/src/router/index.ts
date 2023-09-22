f80663f98d75fbaac3f96baf6147d3dcb6be050f
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-22 12:39:13 +0200

    edit profile view

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -90,6 +90,16 @@ const routes = [
                 },
             },
             {
+                path: "/profile/edit",
+                name: "ProfileEdit",
+                component: () => import("@/views/auth/ProfileEdit.view.vue"),
+                meta: {
+                    middleware: [
+                        requiresAuth
+                    ]
+                },
+            },
+            {
                 path: "/error",
                 name: "Error",
                 component: () => import("@/views/Error.view.vue"),
