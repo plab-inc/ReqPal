cbbe41b5f1ffbf6a42e9819131ff9152b44de044
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-22 12:39:13 +0200

    edit profile view

--- app/src/components/Profile/UserData/UserEdit.component.vue
+++ app/src/components/Profile/UserData/UserEdit.component.vue
@@ -1,16 +1,46 @@
 <template>
-  <div class="profileContainerRow">
-
-  </div>
+  <v-container>
+    <v-form v-model="isFormValid">
+      <v-row>
+        <v-col class="d-flex flex-column align-center justify-center">
+          <v-avatar
+              color="secondary"
+              image="@/assets/images/profile-test.jpg"
+              alt="profilePicture"
+              size="160"></v-avatar>
+          <v-btn>Change Picture</v-btn>
+        </v-col>
+        <v-col class="d-flex flex-column align-center justify-center">
+          <v-text-field label="Username" v-model="userName"></v-text-field>
+          <v-text-field label="E-Mail" v-model="email"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 import {useAuthStore} from "@/stores/auth.store";
 import {User} from "@supabase/supabase-js";
+import {ref} from "vue";
+import {requiredRule} from "@/utils/validationRules";
 
 const authStore = useAuthStore();
 const user: User | null = authStore.user;
 
+const isFormValid = ref(false);
+const rules = {
+  required: requiredRule
+};
+
+let userName: string = '';
+let email: string = '';
+onBeforeMount(() => {
+  if (user && user.email) {
+    userName = user.user_metadata.username;
+    email = user.email;
+  }
+})
 </script>
 <style scoped>
 </style>
---
+++ app/src/views/auth/ProfileEdit.view.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>Edit Profile</h1>
+    <UserEdit></UserEdit>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import UserEdit from "@/components/Profile/UserData/UserEdit.component";
+</script>
