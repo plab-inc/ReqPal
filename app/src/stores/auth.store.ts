87b6fa9fa411ca095a3d102874e550549b785bcb
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-09-26 13:15:41 +0200

    add auth and profile service

--- app/src/stores/auth.store.ts
+++ app/src/stores/auth.store.ts
@@ -1,59 +1,42 @@
-import { defineStore } from 'pinia';
-import { supabase } from '@/plugins/supabase';
-import { Session, User } from '@supabase/supabase-js'
+import {defineStore} from 'pinia';
+import {Session, User} from '@supabase/supabase-js'
+import authService from "@/services/database/auth.service.ts";
 
 interface AuthState {
-  user: User | null;
-  session: Session | null;
+    user: User | null;
+    session: Session | null;
 }
 
 export const useAuthStore = defineStore('auth', {
-  state: (): AuthState => ({
-    user: null,
-    session: null,
-  }),
-  actions: {
-    setSession(session: Session | null) {
-      this.session = session;
-      this.user = session?.user ?? null;
-    },
-    async signIn(email: string, password: string) {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      });
-      if (error) throw error;
+    state: (): AuthState => ({
+        user: null,
+        session: null,
+    }),
+    actions: {
+        setSession(session: Session | null) {
+            this.session = session;
+            this.user = session?.user ?? null;
+        },
+        async signIn(email: string, password: string) {
+            const data = await authService.pull.signInWithPassword(email, password);
 
-      this.user = data.user;
-      this.session = data.session;
-    },
-    async signUp(email: string, password: string, username: string) {
-      const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-        options: {
-          data: {
-            username: username
-          }
-        }
-      });
-      if (error) throw error;
+            this.user = data.user;
+            this.session = data.session;
+        },
+        async signUp(email: string, password: string, username: string) {
+            const data = await authService.push.signUp(email, password, username);
 
-      this.user = data.user;
-      this.session = data.session;
-    },
-    async signOut() {
-      const { error } = await supabase.auth.signOut();
-      if (error) throw error;
+            this.user = data.user;
+            this.session = data.session;
+        },
+        async signOut() {
+            await authService.pull.signOut();
 
-      this.user = null;
-      this.session = null;
+            this.user = null;
+            this.session = null;
+        },
+        async resetPassword(email: string) {
+            await authService.push.resetPassword(email);
+        },
     },
-    async resetPassword(email: string) {
-      const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'https://google.com/',
-      });
-      if (error) throw error;
-    },
-  },
 });
