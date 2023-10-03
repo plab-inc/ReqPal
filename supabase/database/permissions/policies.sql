42c8c6b17b51150aa9304b2994798155be02d6f5
Fabian Teufel <fabianteufel@me.com>
2023-10-03 13:48:29 +0200

    Change some fn param names

--- supabase/database/permissions/policies.sql
+++ supabase/database/permissions/policies.sql
@@ -1,3 +1,14 @@
+DROP POLICY IF EXISTS "policy_profiles" ON public.profiles;
+DROP POLICY IF EXISTS "policy_user_points" ON public.user_points;
+DROP POLICY IF EXISTS "policy_catalogs" ON public.catalogs;
+DROP POLICY IF EXISTS "policy_products" ON public.products;
+DROP POLICY IF EXISTS "policy_requirements" ON public.requirements;
+DROP POLICY IF EXISTS "policy_product_requirements" ON public.product_requirements;
+DROP POLICY IF EXISTS "policy_lessons" ON public.lessons;
+DROP POLICY IF EXISTS "policy_questions" ON public.questions;
+DROP POLICY IF EXISTS "policy_questions_teacher" ON public.questions;
+
+
 --------------------------------------------
 -- User related policies
 --------------------------------------------
