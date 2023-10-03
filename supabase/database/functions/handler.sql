bcdaf97b599e8f14101704178d3ec83eaf2f587c
Fabian Teufel <fabianteufel@me.com>
2023-10-03 10:44:55 +0200

    Run update_user_permissions for new users

--- supabase/database/functions/handler.sql
+++ supabase/database/functions/handler.sql
@@ -9,6 +9,7 @@ begin
     values (new.id, new.raw_user_meta_data ->> 'username');
 
     perform set_claim(new.id, 'userroles', '{"roles": ["student"]}');
+    perform update_user_permissions(new.id);
 
     return new;
 end;
