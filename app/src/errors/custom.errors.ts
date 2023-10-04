d2bbb4770cf83016a5874572135e825d35f22bd3
Fabian Teufel <fabianteufel@me.com>
2023-10-04 16:15:12 +0200

    Use of custom error handling

---
+++ app/src/errors/custom.errors.ts
@@ -0,0 +1,28 @@
+class PrivilegeError extends Error {
+    private code: number;
+    constructor(message:string , code: number) {
+        super(message);
+        this.name = "PrivilegeError";
+        this.code = code;
+    }
+}
+
+class DatabaseError extends Error {
+    private code: number;
+    constructor(message:string , code: number) {
+        super(message);
+        this.name = "DatabaseError";
+        this.code = code;
+    }
+}
+
+class ConversionError extends Error {
+    private code: number;
+    constructor(message:string , code: number) {
+        super(message);
+        this.name = "ConversionError";
+        this.code = code;
+    }
+}
+
+export { PrivilegeError, DatabaseError, ConversionError };
