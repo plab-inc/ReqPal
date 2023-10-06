dd3744fa6489001185b8d8f05ac986b7573118f1
Laura <seifert.laura@gmx.de>
2023-10-06 18:26:03 +0200

    set new requirements to lesson

--- app/src/components/Catalogs/catalog.component.vue
+++ app/src/components/Catalogs/catalog.component.vue
@@ -2,14 +2,24 @@
 import {useCatalogStore} from "@/stores/catalog.store.ts";
 import {Catalog, Requirement} from "@/types/catalog.types.ts";
 import AlertService from "@/services/alert.service.ts";
+import {useLessonStore} from "@/stores/lesson.store.ts";
+import {Lesson} from "@/types/lesson.types.ts";
+import router from "@/router";
 
 const catalog = ref<Catalog>();
 const catalogStore = useCatalogStore();
+
+const lessonStore = useLessonStore();
+
 const loading = ref<boolean>(true);
 const items = ref<Requirement[]>([]);
 const themeColor = "#6e4aff";
 const itemsSelected = ref([]);
 const selectedProduct = ref();
+const lessons = ref<Lesson[]>();
+const selectedLesson = ref<Lesson>();
+
+const isFormValid = ref(false);
 
 const headers = [
   {text: 'ID', value: 'requirement_id', sortable: true},
@@ -22,6 +32,23 @@ const headers = [
 
 function onSelectProduct(name: string) {
   selectedProduct.value = name;
+}
+
+async function onSubmit() {
+
+  if (isFormValid && itemsSelected.value.length > 0 && selectedLesson.value) {
+    console.log(itemsSelected.value)
+    console.log(selectedLesson.value)
+    console.log("submit")
+
+    try {
+      await catalogStore.setCatalogAndRequirementsToLesson(selectedLesson.value.id, itemsSelected.value);
+      AlertService.addSuccessAlert("Requirements added to " + selectedLesson.value?.id + " " + selectedLesson.value?.title);
+      await router.push({name: "AllLessons"})
+    } catch (error: any) {
+      AlertService.addErrorAlert("Failed to add catalog and requirements: " + error.message);
+    }
+  }
 }
 
 onBeforeMount(async () => {
@@ -44,66 +71,88 @@ onBeforeMount(async () => {
     loading.value = false;
     AlertService.addWarningAlert("Kein Katalog gefunden mit der id: " + catalogId);
   }
+
+  if (lessonStore.lessons) {
+    lessons.value = lessonStore.lessons;
+  } else {
+    lessons.value = [];
+    AlertService.addWarningAlert("Keine Lektionen gefunden.");
+  }
 })
 </script>
 
 <template>
   <h1>{{ catalog?.catalog_name }}</h1>
 
-  <v-item-group mandatory>
-    <v-container>
-      <v-row>
-        <v-col
-            :md="2" class="text-h5 d-flex justify-start align-center">
-          Choose details for product:
-        </v-col>
-        <v-col
-            v-for="product in catalog?.products"
-            :key="product.product_id"
-            :md="2"
-        >
-          <v-item v-slot="{ isSelected, toggle }">
-            <v-card
-                :color="isSelected ? 'primary' : ''"
-                class="d-flex align-center"
-                dark
-                height="75"
-                @click="() => { if (toggle) { toggle(); } onSelectProduct(product.product_name); }"
-            >
-              <v-scroll-y-transition>
-                <div
-                    class="text-h5 flex-grow-1 text-center"
-                >
-                  {{ product.product_name }}
-                </div>
-              </v-scroll-y-transition>
-            </v-card>
-          </v-item>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-item-group>
+  <v-form v-model="isFormValid" @submit.prevent="onSubmit" fast-fail>
+    <v-item-group mandatory>
+      <v-container>
+        <v-row>
+          <v-col
+              :md="2" class="text-h5 d-flex justify-start align-center">
+            Choose details for product:
+          </v-col>
+          <v-col
+              v-for="product in catalog?.products"
+              :key="product.product_id"
+              :md="2"
+          >
+            <v-item v-slot="{ isSelected, toggle }">
+              <v-card
+                  :color="isSelected ? 'primary' : ''"
+                  class="d-flex align-center"
+                  dark
+                  height="75"
+                  @click="() => { if (toggle) { toggle(); } onSelectProduct(product.product_name); }"
+              >
+                <v-scroll-y-transition>
+                  <div
+                      class="text-h5 flex-grow-1 text-center"
+                  >
+                    {{ product.product_name }}
+                  </div>
+                </v-scroll-y-transition>
+              </v-card>
+            </v-item>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-item-group>
 
-  <EasyDataTable
-      v-model:itemsSelected="itemsSelected"
-      :headers="headers"
-      :items="items"
-      :loading="loading"
-      :theme-color="themeColor"
-      :rows-items="[5, 10, 15, 25, 50]"
-      :rows-per-page="10"
-      table-class-name="customize-table">
+    <v-btn type="submit" class="my-2 pa-1">
+      Add Requirements to Lesson {{ selectedLesson?.title }}
+    </v-btn>
 
-    <template #item-productQualification="item">
-      {{ item.products[selectedProduct]?.qualification }}
-    </template>
+    <v-select
+        v-model="selectedLesson"
+        :items="lessons"
+        :rules="[v => !!v || 'Eine Lektion wird benötigt.']"
+        label="Lesson"
+        :item-title="item => item.title"
+        :item-value="item => item"
+        required
+    ></v-select>
 
-    <template #item-productComment="item">
-      {{ item.products[selectedProduct]?.comment }}
-    </template>
+    <p v-if="!isFormValid">Es müssen Requirements gewählt werden.</p>
+    <EasyDataTable
+        v-model:itemsSelected="itemsSelected"
+        :headers="headers"
+        :items="items"
+        :loading="loading"
+        :theme-color="themeColor"
+        :rows-items="[5, 10, 15, 25, 50]"
+        :rows-per-page="10"
+        table-class-name="customize-table">
 
-  </EasyDataTable>
+      <template #item-productQualification="item">
+        {{ item.products[selectedProduct]?.qualification }}
+      </template>
 
+      <template #item-productComment="item">
+        {{ item.products[selectedProduct]?.comment }}
+      </template>
+    </EasyDataTable>
+  </v-form>
 </template>
 
 <style scoped>
