d3ce49c999cef101574e3b7a09f6fb9f8f4c921a
Laura <seifert.laura@gmx.de>
2023-10-11 13:15:54 +0200

    remove unused import

--- app/src/views/lesson/Lessons.view.vue
+++ app/src/views/lesson/Lessons.view.vue
@@ -32,7 +32,6 @@
 <script setup lang="ts">
 
 import {useLessonStore} from "@/stores/lesson.store";
-import MultipleChoiceForm from "@/components/MultipleChoice/MultipleChoiceForm.component.vue";
 
 const lessonStore = useLessonStore();
 const lessons = lessonStore.lessons;
