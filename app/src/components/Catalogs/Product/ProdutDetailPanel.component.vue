3bbff6d601e9ebdd9354fdad003961779ad5f4fa
Laura <seifert.laura@gmx.de>
2023-10-12 15:16:19 +0200

    outsource product detail panel

---
+++ app/src/components/Catalogs/Product/ProdutDetailPanel.component.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+import {Product, Requirement} from "@/types/catalog.types.ts";
+import ProductDetailItem from "@/components/Catalogs/Product/ProductDetailItem.component.vue"
+
+interface Props {
+  requirement: Requirement | undefined,
+  products: Product[],
+  loading: boolean
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel v-if="requirement">
+      <v-expansion-panel-title>
+        Produktdetails zur Anforderung
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-row v-if="requirement">
+          <v-col v-for="product in products" :key="product.product_name" cols="12" md="6" lg="4">
+            <ProductDetailItem :requirement="requirement" :loading="loading" :product="product"></ProductDetailItem>
+          </v-col>
+        </v-row>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
