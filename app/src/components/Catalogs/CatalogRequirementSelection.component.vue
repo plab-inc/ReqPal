94bd1f8b1f5323b85df87874d044d8481317b0f3
Laura <seifert.laura@gmx.de>
2023-10-14 15:42:56 +0200

    select catalog and requirement components

---
+++ app/src/components/Catalogs/CatalogRequirementSelection.component.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import {Requirement} from "@/types/catalog.types.ts";
+import {useCatalogStore} from "@/stores/catalog.store.ts";
+import CatalogSelect from "@/components/Catalogs/CatalogSelect.component.vue";
+import RequirementSelect from "@/components/Catalogs/Requirement/RequirementSelect.component.vue";
+
+const catalogId = ref<number>();
+const requirementId = ref<number>();
+const requirements = ref<Requirement[]>([]);
+const catalogStore = useCatalogStore();
+const loadingReqs = ref<boolean>(false);
+
+async function onCatalogChange() {
+  if (catalogId.value) {
+    toggleLoadingReqs();
+    await catalogStore.getCatalogWithProductsById(catalogId.value);
+    if (catalogStore.currentCatalog) {
+      requirements.value = catalogStore.currentCatalog.requirements;
+    }
+    toggleLoadingReqs();
+  }
+}
+
+function toggleLoadingReqs() {
+  loadingReqs.value = !loadingReqs.value;
+}
+
+watch(catalogId, (newCatalogId, oldCatalogId) => {
+  if (newCatalogId !== oldCatalogId) {
+    onCatalogChange();
+  }
+});
+
+</script>
+
+<template>
+  <CatalogSelect v-model="catalogId"></CatalogSelect>
+  <RequirementSelect v-model="requirementId" :loading="loadingReqs" :items="requirements"></RequirementSelect>
+</template>
+
+<style scoped>
+
+</style>
