b0da70137339ef6c39f0213cab81965f86768906
Fabian Teufel <fabianteufel@me.com>
2023-10-14 02:40:38 +0200

    LessonBuilder base

---
+++ app/src/components/LessonBuilder/LessonBuilder.component.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+import { useDrop } from 'vue3-dnd'
+import { DragItemTypes } from '@/types/dragItem.types.ts'
+import { toRefs } from '@vueuse/core'
+import { useTheme } from "vuetify";
+
+import TrueOrFalse from "@/components/TrueOrFalse/TrueOrFalseForm.component.vue";
+import RequirementItem from "@/components/Catalogs/Requirement/RequirementItem.component.vue";
+import {LessonBuilderDragItem} from "@/interfaces/DragItems.interfaces.ts";
+
+interface ComponentEntry {
+  id: number;
+  component: Component;
+}
+
+type ComponentsMap = {
+  [key: string]: Component;
+};
+
+const themeColors = useTheme().current.value.colors;
+
+const componentsMap: ComponentsMap = {
+  'True Or False': TrueOrFalse,
+  'Requirement': RequirementItem,
+};
+
+const components = ref<ComponentEntry[]>([]);
+
+let nextId = 1;
+
+const addComponentByName = (name: string) => {
+  const componentToAdd = componentsMap[name];
+
+  if (componentToAdd) {
+    components.value.push({
+      id: nextId++,
+      component: componentToAdd,
+    });
+  } else {
+    console.error(`Component with name ${name} not found!`);
+  }
+};
+
+const removeComponentById = (id: number) => {
+  components.value = components.value.filter((component) => component.id !== id);
+};
+
+const clearComponents = () => {
+  components.value = [];
+};
+
+defineExpose({
+  clearComponents,
+})
+
+const [collect, drop] = useDrop(() => ({
+  accept: DragItemTypes.COMPONENT,
+  drop: (item: LessonBuilderDragItem) => {
+    addComponentByName(item.name);
+  },
+  collect: monitor => ({
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop(),
+  }),
+}));
+
+const { canDrop, isOver } = toRefs(collect);
+const isActive = computed(() => unref(canDrop) && unref(isOver));
+
+</script>
+
+<template>
+    <div
+        :ref="drop"
+        class="container"
+        :style="{
+        borderColor: isActive ? themeColors.success : themeColors.primary,
+       }"
+    >
+      <v-container>
+        <v-container class="scrollable-rows" v-if="components.length">
+          <v-row v-for="componentEntry in components">
+            <v-col cols="1" align-self="center">
+              <v-btn
+                  class="ma-2"
+                  icon="mdi-delete"
+                  @click="removeComponentById(componentEntry.id)"
+              ></v-btn>
+            </v-col>
+            <v-col cols="11">
+              <v-sheet class="pa-5" rounded>
+                <component :is="componentEntry.component"></component>
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-container>
+          <v-row>
+            <v-col v-if="!components.length">
+              <v-icon icon="mdi-tools"></v-icon>
+            </v-col>
+            <v-col>
+              {{ !components.length ? "Füge Lernmodule hinzu indem du sie aus der Rechten Spalte herziehst" : "Füge weitere Lernmodule hinzu indem du sie aus der Rechten Spalte herziehst"}}
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
+    </div>
+</template>
+
+<style scoped>
+
+.scrollable-rows {
+  max-height: 750px;
+  overflow-y: auto;
+}
+
+.container {
+  display: flex;
+  border: 1px solid;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  min-height: 750px;
+}
+
+</style>
---
+++ app/src/views/lesson/LessonBuilder.view.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+  import LessonBuilder from "@/components/LessonBuilder/LessonBuilder.component.vue";
+  import {HTML5Backend} from "react-dnd-html5-backend";
+  import {DndProvider} from "vue3-dnd";
+  import LessonModuleBox from "@/components/LessonBuilder/LessonModuleBox.component.vue";
+
+  const templates = ['Requirement','True Or False']
+  const LessonBuilderRef = ref<InstanceType<typeof LessonBuilder> | null>(null);
+
+  const invokeResetLessonBuilder = () => {
+    LessonBuilderRef.value?.clearComponents();
+  };
+
+</script>
+
+<template>
+  <DndProvider :backend="HTML5Backend">
+    <v-container>
+      <v-row>
+        <v-col cols="10">
+          <LessonBuilder ref="LessonBuilderRef"/>
+        </v-col>
+        <v-col cols="2">
+          <v-card>
+            <v-container>
+              <v-row>
+                <v-col v-for="template in templates" :key="template">
+                  <LessonModuleBox :title="template"/>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-btn block @click="invokeResetLessonBuilder">Reset</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn block disabled>Save</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </DndProvider>
+</template>
