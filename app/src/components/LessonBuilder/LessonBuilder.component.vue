e6448a7f46a5318320b518814ec3043755d33ede
Fabian Teufel <fabianteufel@me.com>
2023-10-14 18:19:01 +0200

    Sore for form data, updated ToF question form

--- app/src/components/LessonBuilder/LessonBuilder.component.vue
+++ app/src/components/LessonBuilder/LessonBuilder.component.vue
@@ -1,63 +1,39 @@
 <script setup lang="ts">
-import { useDrop } from 'vue3-dnd'
-import { DragItemTypes } from '@/types/dragItem.types.ts'
-import { toRefs } from '@vueuse/core'
+import { useDrop } from 'vue3-dnd';
+import { DragItemTypes } from '@/types/dragItem.types.ts';
+import { toRefs } from '@vueuse/core';
 import { useTheme } from "vuetify";
+import {useLessonFormStore} from "@/stores/lessonForm.store";
 
 import TrueOrFalse from "@/components/TrueOrFalse/TrueOrFalseForm.component.vue";
-import CatalogRequirementSelection from "@/components/Catalogs/CatalogRequirementSelection.component.vue";
-import {LessonBuilderDragItem} from "@/interfaces/DragItems.interfaces.ts";
-import TextInput from "@/components/Notes/TextInput.component.vue"
-interface ComponentEntry {
-  id: number;
-  component: Component;
-}
+import RequirementItem from "@/components/Catalogs/Requirement/RequirementItem.component.vue";
+import MultipleChoiceForm from "@/components/MultipleChoice/MultipleChoiceForm.component.vue";
 
-type ComponentsMap = {
+import { LessonBuilderDragItem } from "@/interfaces/DragItems.interfaces.ts";
+
+interface ComponentsMap {
   [key: string]: Component;
-};
+}
 
 const themeColors = useTheme().current.value.colors;
 
 const componentsMap: ComponentsMap = {
-  'True Or False': TrueOrFalse,
-  'Requirement': CatalogRequirementSelection,
-  'Textfeld': TextInput
+  'TrueOrFalse': markRaw(TrueOrFalse),
+  'Requirement': markRaw(RequirementItem),
+  'Multiple Choice': markRaw(MultipleChoiceForm),
 };
 
-const components = ref<ComponentEntry[]>([]);
+const lessonBuilderStore = useLessonFormStore();
+const components = lessonBuilderStore.components;
 
-let nextId = 1;
-
-const addComponentByName = (name: string) => {
-  const componentToAdd = componentsMap[name];
-
-  if (componentToAdd) {
-    components.value.push({
-      id: nextId++,
-      component: componentToAdd,
-    });
-  } else {
-    console.error(`Component with name ${name} not found!`);
-  }
+const getComponentInstance = (componentName: string): Component => {
+  return componentsMap[componentName];
 };
-
-const removeComponentById = (id: number) => {
-  components.value = components.value.filter((component) => component.id !== id);
-};
-
-const clearComponents = () => {
-  components.value = [];
-};
-
-defineExpose({
-  clearComponents,
-})
 
 const [collect, drop] = useDrop(() => ({
   accept: DragItemTypes.COMPONENT,
   drop: (item: LessonBuilderDragItem) => {
-    addComponentByName(item.name);
+    lessonBuilderStore.addComponent(item.name);
   },
   collect: monitor => ({
     isOver: monitor.isOver(),
@@ -71,46 +47,51 @@ const isActive = computed(() => unref(canDrop) && unref(isOver));
 </script>
 
 <template>
-    <div
-        :ref="drop"
-        class="container"
-        :style="{
-        borderColor: isActive ? themeColors.success : themeColors.primary,
-       }"
-    >
-      <v-container>
-        <v-container class="scrollable-rows" v-if="components.length">
+  <div
+      :ref="drop"
+      class="container"
+      :style="{
+      borderColor: isActive ? themeColors.success : themeColors.primary,
+    }"
+  >
+    <v-container>
+      <v-container class="scrollable-rows" v-if="components">
+        <v-form @submit.prevent>
           <v-row v-for="componentEntry in components">
             <v-col cols="1" align-self="center">
               <v-btn
                   class="ma-2"
                   icon="mdi-delete"
-                  @click="removeComponentById(componentEntry.id)"
+                  @click="lessonBuilderStore.removeComponentById(componentEntry.id)"
               ></v-btn>
             </v-col>
             <v-col cols="11">
               <v-sheet class="pa-5" rounded>
-                <component :is="componentEntry.component"></component>
+                <component
+                    :is="getComponentInstance(componentEntry.name)"
+                    :key="componentEntry.id"
+                    :componentId="componentEntry.id"
+                ></component>
               </v-sheet>
             </v-col>
           </v-row>
-        </v-container>
-        <v-container>
-          <v-row>
-            <v-col v-if="!components.length">
-              <v-icon icon="mdi-tools"></v-icon>
-            </v-col>
-            <v-col>
-              {{ !components.length ? "Füge Lernmodule hinzu indem du sie aus der Rechten Spalte herziehst" : "Füge weitere Lernmodule hinzu indem du sie aus der Rechten Spalte herziehst"}}
-            </v-col>
-          </v-row>
-        </v-container>
+        </v-form>
       </v-container>
-    </div>
+      <v-container>
+        <v-row>
+          <v-col v-if="!components.length">
+            <v-icon icon="mdi-tools"></v-icon>
+          </v-col>
+          <v-col>
+            {{ !components.length ? "Füge Lernmodule hinzu indem du sie aus der Rechten Spalte herziehst" : "Füge weitere Lernmodule hinzu indem du sie aus der Rechten Spalte herziehst" }}
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+  </div>
 </template>
 
 <style scoped>
-
 .scrollable-rows {
   max-height: 750px;
   overflow-y: auto;
@@ -125,5 +106,4 @@ const isActive = computed(() => unref(canDrop) && unref(isOver));
   text-align: center;
   min-height: 750px;
 }
-
 </style>
