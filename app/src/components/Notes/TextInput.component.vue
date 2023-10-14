4f825de26f6b8659eeac04b58e71705f95a63c20
Laura <seifert.laura@gmx.de>
2023-10-14 17:11:56 +0200

    add text input component

---
+++ app/src/components/Notes/TextInput.component.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Notes from "@/components/Notes/Notes.component.vue";
+import {requiredRule} from "@/utils/validationRules.ts";
+
+const text = ref<string>("");
+const rules = {
+  required: requiredRule
+};
+</script>
+
+<template>
+  <div class="text-subtitle-1">{{ 'Füge hier einen Titel oder eine Beschreibung zur Aufgabe hinzu.' }}</div>
+  <Notes v-model="text" :rules="[rules.required]" :textLabel="'Beschreibung'"></Notes>
+</template>
