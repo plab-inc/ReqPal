81cc65dd2d745b92c3d1ef1f469d0ca332b8befc
Laura <seifert.laura@gmx.de>
2023-10-14 19:46:17 +0200

    save textfield data in lessonFormStore, add new no empty string rule

--- app/src/components/Notes/TextInput.component.vue
+++ app/src/components/Notes/TextInput.component.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
-import Notes from "@/components/Notes/Notes.component.vue";
-import {requiredRule} from "@/utils/validationRules.ts";
+import {noEmptyStringRule, requiredRule} from "@/utils/validationRules.ts";
+import {useLessonFormStore} from "@/stores/lessonForm.store.ts";
 
 const text = ref<string>("");
-const rules = {
-  required: requiredRule
-};
+const props = defineProps<{ componentId: number }>();
+const lessonFormStore = useLessonFormStore();
+
+watch(text, (newText) => {
+  lessonFormStore.setComponentData(props.componentId, 'question', newText);
+}, {deep: true});
+
+onBeforeMount(() => {
+  const data = lessonFormStore.getComponentFieldValues(props.componentId, 'question');
+  if (typeof data === 'string') {
+    text.value = data;
+  }
+})
 </script>
 
 <template>
   <div class="text-subtitle-1">{{ 'Füge hier einen Titel oder eine Beschreibung zur Aufgabe hinzu.' }}</div>
-  <Notes v-model="text" :rules="[rules.required]" :textLabel="'Beschreibung'"></Notes>
+  <v-textarea
+      v-model="text"
+      :rules="[noEmptyStringRule]"
+      label="Beschreibung"
+      variant="outlined"
+      auto-grow></v-textarea>
 </template>
