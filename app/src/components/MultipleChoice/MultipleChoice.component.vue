b521b2fdd01bbf3ee75aca8fce43a4803499802f
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-10-15 22:06:05 +0200

    add props to components

--- app/src/components/MultipleChoice/MultipleChoice.component.vue
+++ app/src/components/MultipleChoice/MultipleChoice.component.vue
@@ -1,66 +1,51 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+type Option = { id: number, description: string }
+type Solution = { id: number, solution: boolean }
+
+interface Props {
+  componentId: number,
+  question: string,
+  options: Option[] | any,
+  solution: Solution[] | any | undefined
+}
+
+const props = defineProps<Props>();
+
+const selectedAnswers = ref<Option[]>([]);
+
+function checkSolution(id: number) {
+  if (typeof props.solution === 'object' && Array.isArray(props.solution)) {
+    const found = props.solution.find(s => s.id === id);
+    if (found) {
+      return found.solution;
+    } else return undefined;
+  }
+}
+</script>
+
 <template>
   <v-card>
     <v-container>
       <div class="text-h6 text-md-h5">Multiple Choice</div>
+      <v-form fast-fail>
+        <div class="text-h6">{{ question }}</div>
+        <div class="text-h6" v-if="solution">Lösung:</div>
 
-      <v-form v-if="answers" @submit.prevent="submitAnswers" fast-fail>
-
-        <div class="text-h6">{{ props.question.description }}</div>
-        <div class="text-h6" v-if="submitted">Solution:</div>
-
-        <v-checkbox v-for="(answer, index) in answers"
-                    :key="index"
+        <v-checkbox v-for="(answer, index) in options"
+                    :key="answer.id"
                     :label="answer.description"
                     v-model="selectedAnswers[index]"
-                    :class="{ 'right': submitted && (props.question.userResults?.wholeAnswerIsCorrect || (props.question.userResults?.results[index]?.answerIsCorrect ?? false)),
-                'disabled': submitted,
-                'wrong': submitted && !(props.question.userResults?.results[index]?.answerIsCorrect ?? false)}">
+                    :class="{ 'right': (solution && checkSolution(answer.id)),
+                'disabled': solution,
+                'wrong': (solution && !checkSolution(answer.id))}">
         </v-checkbox>
-
-        <v-btn type="submit" :disabled="submitted">Submit</v-btn>
       </v-form>
     </v-container>
   </v-card>
 </template>
-
-<script setup lang="ts">
-
-import {ref} from "vue";
-import {useLessonStore} from "@/stores/lesson.store";
-import {Answer, Question} from "@/types/lesson.types";
-
-interface Props {
-  question: Question;
-}
-
-const props = defineProps<Props>();
-const selectedAnswers = ref<boolean[]>([]);
-const submitted = ref(false);
-const lessonStore = useLessonStore();
-const answers = ref<Answer[]>();
-
-async function submitAnswers(): Promise<void> {
-  if (submitted.value) return;
-  if (answers.value) {
-    const userAnswers = answers.value.map((answer, index) => {
-      return {
-        id: answer.id,
-        description: answer.description,
-        solution: selectedAnswers.value[index],
-      };
-    });
-
-    await lessonStore.compareUserMCAnswers(userAnswers, props.question.id);
-    submitted.value = true;
-  }
-}
-
-onBeforeMount(async () => {
-  answers.value = await lessonStore.fetchMCAnswersForQuestion(props.question.id);
-  if (answers.value) selectedAnswers.value = answers.value.map(() => false);
-});
-
-</script>
 
 <style scoped>
 .right {
