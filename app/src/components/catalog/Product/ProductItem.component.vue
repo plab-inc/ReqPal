e1e758670cbde77fd2022ea8fb9d4e556e3bd209
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-10-17 01:21:11 +0200

    use question and lesson from store

--- app/src/components/catalog/Product/ProductItem.component.vue
+++ app/src/components/catalog/Product/ProductItem.component.vue
@@ -8,27 +8,30 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <v-card>
-    <v-card-title>
-      <v-container>
-        <v-row>
-          <v-col class="d-flex justify-start align-center">
-            <v-icon :icon="'mdi-'+options.icon.toLowerCase()" size="50px" class="mr-2"></v-icon>
-            <div class="text-h4">{{ options.name }}</div>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card-title>
-
-    <v-card-actions>
-      <v-btn
-          variant="text"
-          color="info"
-      >
-        Mehr Infos: {{ options.link }}
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-hover>
+    <template v-slot:default="{ isHovering, props }">
+      <v-card
+          variant="flat"
+          v-bind="props"
+          :color="isHovering ? 'info' : undefined">
+        <v-card-title>
+          <v-container>
+            <v-row>
+              <v-col class="d-flex justify-start align-center">
+                <v-icon :icon="options.icon.toLowerCase()" size="50px" class="mr-2"></v-icon>
+                <div class="text-h4">{{ options.name }}</div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-title>
+        <v-card-text>
+          <v-chip prepend-icon="mdi-web" variant="text">
+            {{ options.link }}
+          </v-chip>
+        </v-card-text>
+      </v-card>
+    </template>
+  </v-hover>
 </template>
 
 <style scoped>
