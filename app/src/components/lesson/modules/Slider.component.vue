70c2aaaa7e157071d316d1a0c509f2c02c7534ec
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-10-17 21:45:04 +0200

    use lessonFormStore

--- app/src/components/lesson/modules/Slider.component.vue
+++ app/src/components/lesson/modules/Slider.component.vue
@@ -1,21 +1,47 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import Hint from "@/components/lesson/modules/Hint.component.vue";
+import {useLessonFormStore} from "@/stores/lessonForm.store.ts";
 
 interface Props {
-  componentId: number,
-  question: string | any,
-  options: { minValue: number, maxValue: number, steps: number } | any,
-  hint: string | any
+  componentId: string,
 }
 
 const props = defineProps<Props>();
 
 const inputValue = ref<number>(0);
 
-onBeforeMount(() => {
-  inputValue.value = (props.options.maxValue / 2);
-})
+const lessonFormStore = useLessonFormStore();
+const question = lessonFormStore.getComponentFieldValues(props.componentId, 'question')
+const hint = lessonFormStore.getComponentFieldValues(props.componentId, 'hint')
+
+const fields = ref<any>({
+  options: lessonFormStore.getComponentFieldValues(props.componentId, 'options'),
+});
+
+function updateStoreData(fields: any) {
+  lessonFormStore.setComponentData(props.componentId, 'options', fields.options);
+}
+
+init();
+
+function init() {
+  if (fields.value.options.hasOwnProperty('input')) {
+    inputValue.value = fields.value.options.input;
+  } else {
+    inputValue.value = (fields.value.options.maxValue / 2);
+  }
+}
+
+watch(inputValue, (newInput) => {
+  fields.value.options = {
+    steps: fields.value.options.steps,
+    minValue: fields.value.options.minValue,
+    maxValue: fields.value.options.maxValue,
+    input: newInput
+  };
+  updateStoreData(fields.value)
+}, {deep: true});
 </script>
 
 <template>
@@ -35,9 +61,9 @@ onBeforeMount(() => {
           <v-form>
             <v-slider
                 v-model="inputValue"
-                :min="options.minValue"
-                :max="options.maxValue"
-                :step="options.steps"
+                :min="fields.options.minValue"
+                :max="fields.options.maxValue"
+                :step="fields.options.steps"
                 color="orange"
                 track-color="warning"
                 thumb-label>
