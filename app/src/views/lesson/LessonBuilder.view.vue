404694f38bd4f846358c7c389197e5e592626540
Fabian Teufel <fabianteufel@me.com>
2023-10-20 01:03:36 +0200

    Builder checks if store is dirty

--- app/src/views/lesson/LessonBuilder.view.vue
+++ app/src/views/lesson/LessonBuilder.view.vue
@@ -3,11 +3,15 @@
   import LessonBuilder from "@/components/lesson/lessonBuilder/LessonBuilder.component.vue";
   import {HTML5Backend} from "react-dnd-html5-backend";
   import {DndProvider} from "vue3-dnd";
+  import {useLessonFormStore} from "@/stores/lessonForm.store.ts";
+
+  const lessonFormStore = useLessonFormStore();
 
 </script>
 
 <template>
-  <h1>Neue Lektion erstellen</h1>
+  <h1 v-if="!lessonFormStore.isDirty">Neue Lektion erstellen</h1>
+  <h1 v-else>Lektion bearbeiten</h1>
   <v-divider></v-divider>
   <DndProvider :backend="HTML5Backend">
     <v-container>
