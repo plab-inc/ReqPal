ab88ec7d00949293022b201795798e5a85388003
Fabian Teufel <fabianteufel@me.com>
2023-10-20 13:53:05 +0200

    Minor type stuff and get_lesson_json dbfn

--- app/src/interfaces/Question.interfaces.ts
+++ app/src/interfaces/Question.interfaces.ts
@@ -1,17 +1,26 @@
+import {inherits} from "@babel/types";
+
 export interface Question {
     [key: string]: any;
     question: string | null;
     options: string[] | null;
-    solution: any | null;
     hint: string | null;
 
+    solution?: any | null;
     position?: number | null;
     type?: string | null;
 }
 
-export interface multipleChoiceAnswer {
+export interface solution {
+    [key: string]: any;
+}
+
+export interface multipleChoiceAnswer extends solution{
     id: number,
     description: string,
+    solution: boolean
+}
+export interface trueOrFalseAnswer extends solution{
     solution: boolean
 }
 
