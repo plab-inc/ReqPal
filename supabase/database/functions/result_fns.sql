bc72ef085c0f9e9d68f1f3e15a67ad7ab253c98a
Laura <seifert.laura@gmx.de>
2023-10-23 19:52:22 +0200

    add points and score to calculation and trigger

--- supabase/database/functions/result_fns.sql
+++ supabase/database/functions/result_fns.sql
@@ -1,58 +1,94 @@
-DROP FUNCTION IF EXISTS evaluate_true_or_false(uuid, uuid, jsonb);
+DROP FUNCTION IF EXISTS evaluate_true_or_false(uuid, jsonb, float);
 DROP FUNCTION IF EXISTS create_user_answers_from_json(jsonb);
 
-CREATE OR REPLACE FUNCTION create_user_answers_from_json(data jsonb)
+CREATE
+    OR REPLACE FUNCTION create_user_answers_from_json(data jsonb)
     RETURNS VOID AS
 $$
 DECLARE
-lesson_uuid uuid;
+    lesson_uuid uuid;
     answer jsonb;
     question_uuid uuid;
     lesson_finished bool := false;
+    max_points int4 := 0;
+    points_per_question int4 := 0;
 BEGIN
     lesson_uuid := data ->> 'uuid';
 
-SELECT finished
-INTO lesson_finished
-FROM user_finished_lessons
-WHERE lesson_id = lesson_uuid;
+    SELECT lessons.points
+    INTO max_points
+    FROM lessons
+    WHERE uuid = lesson_uuid;
 
-IF NOT FOUND THEN
+    points_per_question := max_points / jsonb_array_length(data -> 'answers');
+
+    SELECT finished
+    INTO lesson_finished
+    FROM user_finished_lessons
+    WHERE lesson_id = lesson_uuid AND user_id = auth.uid();
+
+    IF NOT FOUND THEN
         INSERT INTO user_finished_lessons (user_id, lesson_id, finished)
         VALUES (auth.uid(), lesson_uuid, TRUE);
-        lesson_finished := FALSE;
-END IF;
+        lesson_finished
+            := FALSE;
+    END IF;
 
     IF (NOT lesson_finished) THEN
-        FOR answer IN SELECT * FROM jsonb_array_elements(data -> 'answers')
-                                               LOOP
-    question_uuid := (answer ->> 'uuid')::uuid;
-INSERT INTO user_answers (user_id, lesson_id, question_id, answer)
-VALUES (auth.uid(), lesson_uuid, question_uuid, answer -> 'options');
-END LOOP;
-END IF;
+        FOR answer IN
+            SELECT *
+            FROM jsonb_array_elements(data -> 'answers')
+            LOOP
+                question_uuid := (answer ->> 'uuid')::uuid;
+                INSERT INTO user_answers (user_id, lesson_id, question_id, answer, max_points)
+                VALUES (auth.uid(), lesson_uuid, question_uuid, answer -> 'options', points_per_question);
+
+            END LOOP;
+    END IF;
+
+    UPDATE user_finished_lessons
+    SET finished = TRUE
+    WHERE lesson_id = lesson_uuid AND user_id = auth.uid();
 END;
+$$
+    LANGUAGE plpgsql;
 
-$$ LANGUAGE plpgsql;
+--------------------------------------------
+-- EVALUATION FUNCTIONS
+--------------------------------------------
 
-CREATE OR REPLACE FUNCTION evaluate_true_or_false(question_id uuid, answer_id uuid, answer jsonb)  RETURNS jsonb
-AS $$
+CREATE
+    OR REPLACE FUNCTION evaluate_true_or_false(question_id uuid, answer jsonb, max_points int4) RETURNS jsonb
+AS
+$$
 DECLARE
-solution jsonb;
-    compared_result bool;
-    user_result jsonb;
+    solution jsonb;
+    compared_result
+             bool;
+    user_result
+             jsonb;
+    score
+             int4 := 0;
 BEGIN
 
-SELECT questions.solution
-INTO solution
-FROM questions
-WHERE questions.uuid = question_id;
+    SELECT questions.solution
+    INTO solution
+    FROM questions
+    WHERE questions.uuid = question_id;
 
-compared_result := (solution = answer);
+    compared_result
+        := (solution = answer);
 
-    user_result := to_jsonb(compared_result);
+    user_result
+        := to_jsonb(compared_result);
 
-return user_result;
+    IF
+        (user_result) THEN
+        score := max_points;
+    END IF;
+
+    return jsonb_build_object('isCorrect', compared_result, 'score', score);
+
 END;
-$$ LANGUAGE plpgsql;
-
+$$
+    LANGUAGE plpgsql;
