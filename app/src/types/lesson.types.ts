dd669fb76a8f5e8e1275cc8fc814643b8a5f9976
Laura <seifert.laura@gmx.de>
2023-10-23 17:29:35 +0200

    remove warnings

--- app/src/types/lesson.types.ts
+++ app/src/types/lesson.types.ts
@@ -1,5 +1,5 @@
 import {Database} from "@/types/supabase.types.ts";
-import {Question, solution} from "@/interfaces/Question.interfaces.ts";
+import {Question} from "@/interfaces/Question.interfaces.ts";
 
 export type LessonDTO = Database["public"]["Tables"]["lessons"]["Row"];
 export type QuestionDTO = Database["public"]["Tables"]["questions"]["Row"];
