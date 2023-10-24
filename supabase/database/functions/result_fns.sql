84e974cae1ce6e2bf2c2b49648133dc4e063b98f
Laura <seifert.laura@gmx.de>
2023-10-24 11:23:47 +0200

    change data type to double precision and add slider evaluation function

--- supabase/database/functions/result_fns.sql
+++ supabase/database/functions/result_fns.sql
@@ -5,12 +5,12 @@ CREATE
     RETURNS VOID AS
 $$
 DECLARE
-    lesson_uuid uuid;
-    answer jsonb;
-    question_uuid uuid;
-    lesson_finished bool := false;
-    max_points int4 := 0;
-    points_per_question int4 := 0;
+    lesson_uuid         uuid;
+    answer              jsonb;
+    question_uuid       uuid;
+    lesson_finished     bool             := false;
+    max_points          double precision := 0;
+    points_per_question double precision := 0;
 BEGIN
     lesson_uuid := data ->> 'uuid';
 
@@ -24,7 +24,8 @@ BEGIN
     SELECT finished
     INTO lesson_finished
     FROM user_finished_lessons
-    WHERE lesson_id = lesson_uuid AND user_id = auth.uid();
+    WHERE lesson_id = lesson_uuid
+      AND user_id = auth.uid();
 
     IF NOT FOUND THEN
         INSERT INTO user_finished_lessons (user_id, lesson_id, finished)
@@ -47,13 +48,18 @@ BEGIN
 
     UPDATE user_finished_lessons
     SET finished = TRUE
-    WHERE lesson_id = lesson_uuid AND user_id = auth.uid();
+    WHERE lesson_id = lesson_uuid
+      AND user_id = auth.uid();
 END;
 $$
     LANGUAGE plpgsql;
 
 --------------------------------------------
 -- EVALUATION FUNCTIONS
+--------------------------------------------
+
+--------------------------------------------
+-- TRUE OR FALSE
 --------------------------------------------
 DROP FUNCTION IF EXISTS evaluate_true_or_false(uuid, jsonb, double precision);
 
@@ -62,10 +68,10 @@ CREATE
 AS
 $$
 DECLARE
-    solution jsonb;
+    solution        jsonb;
     compared_result bool;
-    user_result jsonb;
-    score int4 := max_points;
+    user_result     jsonb;
+    score           double precision := max_points;
 BEGIN
 
     SELECT questions.solution
@@ -88,21 +94,25 @@ END;
 $$
     LANGUAGE plpgsql;
 
+--------------------------------------------
+-- MULTIPLE CHOICE
+--------------------------------------------
+
 DROP FUNCTION IF EXISTS evaluate_multiple_choice(uuid, jsonb, double precision);
 
 CREATE
-    OR REPLACE FUNCTION evaluate_multiple_choice(question_id uuid, answer jsonb, max_points integer) RETURNS jsonb
+    OR REPLACE FUNCTION evaluate_multiple_choice(question_id uuid, answer jsonb, max_points double precision) RETURNS jsonb
 AS
 $$
 DECLARE
-    solution jsonb;
+    solution        jsonb;
     compared_result jsonb;
-    user_result jsonb;
-    score int4 := 0;
-    percentage float := 1.0;
-    correctAnswers float := 0;
-    totalAnswers float := 0;
-    result jsonb;
+    user_result     jsonb;
+    score           double precision := 0;
+    percentage      double precision := 1.0;
+    correctAnswers  double precision := 0;
+    totalAnswers    double precision := 0;
+    result          jsonb;
 BEGIN
 
     SELECT questions.solution
@@ -111,26 +121,65 @@ BEGIN
     WHERE questions.uuid = question_id;
 
     compared_result := json_agg(jsonb_build_object(
-            'id', q_option->>'id',
-            'isCorrect', (q_option->>'solution')::BOOLEAN = (o_option->>'input')::BOOLEAN
+            'id', q_option ->> 'id',
+            'isCorrect', (q_option ->> 'solution')::BOOLEAN = (o_option ->> 'input')::BOOLEAN
         ))
                        FROM json_array_elements(solution::JSON) AS q_option
-                                LEFT JOIN json_array_elements(answer::JSON) AS o_option ON (q_option->>'id')::int = (o_option->>'id')::int;
+                                LEFT JOIN json_array_elements(answer::JSON) AS o_option
+                                          ON (q_option ->> 'id')::int = (o_option ->> 'id')::int;
 
     FOR result IN SELECT * FROM jsonb_array_elements(compared_result)
         LOOP
-            IF (result->>'isCorrect')::BOOLEAN THEN
+            IF (result ->> 'isCorrect')::BOOLEAN THEN
                 correctAnswers := correctAnswers + 1;
             END IF;
         END LOOP;
 
     totalAnswers := jsonb_array_length(answer);
     percentage := correctAnswers / totalAnswers;
-    score := max_points*percentage;
+    score := max_points * percentage;
 
     user_result := json_build_object('score', score, 'results', compared_result);
     RETURN user_result;
 
+END
+$$
+    LANGUAGE plpgsql;
+
+--------------------------------------------
+-- SLIDER
+--------------------------------------------
+
+DROP FUNCTION IF EXISTS evaluate_slider(uuid, jsonb, double precision);
+
+CREATE
+    OR REPLACE FUNCTION evaluate_slider(question_id uuid, answer jsonb, max_points double precision) RETURNS jsonb
+AS
+$$
+DECLARE
+    solution        jsonb;
+    right_answer    integer;
+    tolerance_value integer;
+    user_input      integer;
+    score           double precision := 0;
+    is_correct      bool             := false;
+BEGIN
+
+    SELECT questions.solution
+    INTO solution
+    FROM questions
+    WHERE questions.uuid = question_id;
+
+    user_input := answer ->> 'input';
+    right_answer := solution ->> 'correctValue';
+    tolerance_value := solution ->> 'toleranceValue';
+
+    IF (user_input <= right_answer + tolerance_value) AND (user_input >= right_answer - tolerance_value) THEN
+        score := max_points;
+        is_correct := true;
+    END IF;
+
+    RETURN json_build_object('score', score, 'isCorrect', is_correct);
 END
 $$
     LANGUAGE plpgsql;
