db1a91f58e4a08ae42ff0b441eb4316ee5aae1f1
Laura <seifert.laura@gmx.de>
2023-10-26 19:20:24 +0200

    user can now restart lesson but not gain new points

--- supabase/database/functions/result_fns.sql
+++ supabase/database/functions/result_fns.sql
@@ -1,25 +1,28 @@
 DROP FUNCTION IF EXISTS create_user_answers_from_json(jsonb);
-
 CREATE
     OR REPLACE FUNCTION create_user_answers_from_json(data jsonb)
     RETURNS VOID AS
 $$
 DECLARE
     lesson_uuid         uuid;
-    answer              jsonb;
+    newAnswer           jsonb;
     question_uuid       uuid;
     lesson_finished     bool             := false;
+    lesson_started    bool             := false;
     max_points          double precision := 0;
     points_per_question double precision := 0;
 BEGIN
     lesson_uuid := data ->> 'uuid';
 
-    SELECT lessons.points
-    INTO max_points
-    FROM lessons
-    WHERE uuid = lesson_uuid;
+    SELECT is_started
+    INTO lesson_started
+    FROM user_finished_lessons
+    WHERE lesson_id = lesson_uuid
+      AND user_id = auth.uid();
 
-    points_per_question := max_points / jsonb_array_length(data -> 'answers');
+    IF NOT FOUND THEN
+        lesson_started := TRUE;
+    end if;
 
     SELECT finished
     INTO lesson_finished
@@ -28,28 +31,49 @@ BEGIN
       AND user_id = auth.uid();
 
     IF NOT FOUND THEN
-        INSERT INTO user_finished_lessons (user_id, lesson_id, finished)
-        VALUES (auth.uid(), lesson_uuid, TRUE);
+        SELECT lessons.points
+        INTO max_points
+        FROM lessons
+        WHERE uuid = lesson_uuid;
+
+        points_per_question := max_points / jsonb_array_length(data -> 'answers');
+
+        INSERT INTO user_finished_lessons (user_id, lesson_id, finished, is_started)
+        VALUES (auth.uid(), lesson_uuid, TRUE, FALSE);
         lesson_finished
             := FALSE;
     END IF;
 
     IF (NOT lesson_finished) THEN
-        FOR answer IN
+        FOR newAnswer IN
             SELECT *
             FROM jsonb_array_elements(data -> 'answers')
             LOOP
-                question_uuid := (answer ->> 'uuid')::uuid;
+                question_uuid := (newAnswer ->> 'uuid')::uuid;
                 INSERT INTO user_answers (user_id, lesson_id, question_id, answer, max_points)
-                VALUES (auth.uid(), lesson_uuid, question_uuid, answer -> 'options', points_per_question);
+                VALUES (auth.uid(), lesson_uuid, question_uuid, newAnswer -> 'options', points_per_question);
 
             END LOOP;
-    END IF;
 
-    UPDATE user_finished_lessons
-    SET finished = TRUE
-    WHERE lesson_id = lesson_uuid
-      AND user_id = auth.uid();
+    ELSE IF(lesson_started) THEN
+        FOR newAnswer IN
+            SELECT *
+            FROM jsonb_array_elements(data -> 'answers')
+            LOOP
+                question_uuid := (newAnswer ->> 'uuid')::uuid;
+                UPDATE user_answers
+                SET answer = newAnswer -> 'options'
+                WHERE lesson_id = lesson_uuid
+                  AND user_id = auth.uid()
+                  AND question_id = question_uuid;
+            END LOOP;
+        UPDATE user_finished_lessons
+        SET finished  = TRUE,
+            is_started = FALSE
+        WHERE lesson_id = lesson_uuid
+          AND user_id = auth.uid();
+    END IF;
+    END IF;
 END;
 $$
     LANGUAGE plpgsql;
@@ -120,7 +144,7 @@ BEGIN
     compared_result := json_agg(jsonb_build_object(
             'id', q_option ->> 'id',
             'isCorrect', (q_option ->> 'solution')::BOOLEAN = (o_option ->> 'input')::BOOLEAN
-        ))
+                                ))
                        FROM json_array_elements(solution::JSON) AS q_option
                                 LEFT JOIN json_array_elements(answer::JSON) AS o_option
                                           ON (q_option ->> 'id')::int = (o_option ->> 'id')::int;
