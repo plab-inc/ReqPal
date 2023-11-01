3c9b8524de526f0a2aafbaa546c980607d8cc3f1
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-11-01 17:27:37 +0100

    update dialogs

--- app/src/components/lesson/modules/Slider.component.vue
+++ app/src/components/lesson/modules/Slider.component.vue
@@ -123,7 +123,7 @@ watch(inputValue, (newInput) => {
       <v-row>
         <v-col class="d-flex flex-grow-1 align-end justify-end">
           <div class="mr-2">
-            <Help dialog-type="mcExplanation"></Help>
+            <Help dialog-type="sliderExplanation"></Help>
           </div>
           <div>
             <Hint v-if="hint" :hint="hint"></Hint>
