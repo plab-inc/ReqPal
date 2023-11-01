2d8a53d37c43885def9455222f2d1be4199b1d6b
Laura <laura.seifert002@stud.fh-dortmund.de>
2023-11-01 17:28:38 +0100

    create own component for product qualification progress circular

---
+++ app/src/components/catalog/product/ProductQualification.component.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+interface Props {
+  qualification: string,
+  size: number
+}
+
+const props = defineProps<Props>();
+
+const getProgressColor = (qualification: string) => {
+  switch (parseInt(qualification)) {
+    case 5:
+      return 'green';
+    case 4:
+      return 'light-green';
+    case 3:
+      return 'yellow';
+    case 2:
+      return 'orange';
+    case 1:
+      return 'red';
+    default:
+      return 'grey'; // default color in case of any unforeseen values
+  }
+};
+</script>
+
+<template>
+  <v-progress-circular
+      v-if="qualification"
+      :size="size"
+      :width="7"
+      :color="getProgressColor(qualification)"
+      :model-value="(parseInt(qualification) / 5 ) * 100"
+  >
+    {{ qualification }}
+  </v-progress-circular>
+</template>
+
+<style scoped>
+
+</style>
