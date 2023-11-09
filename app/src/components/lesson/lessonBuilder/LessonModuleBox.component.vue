8c21832b7bc186ec56d8b9f57c0da17de5119a59
Fabian Teufel <fabianteufel@me.com>
2023-11-09 21:19:26 +0100

    Tooltips for modules and remove of product choice module

--- app/src/components/lesson/lessonBuilder/LessonModuleBox.component.vue
+++ app/src/components/lesson/lessonBuilder/LessonModuleBox.component.vue
@@ -20,8 +20,25 @@ const [collect, drag] = useDrag(() => ({
   }),
 }))
 
+const componentsTranslationMap: Record<string, { title: string, tooltip: string }> = {
+  'TrueOrFalse': {title: 'Wahr oder Falsch', tooltip: 'Eine einfache Frage mit zwei Antwortmöglichkeiten'},
+  'Requirement': {title: 'Anforderung', tooltip: 'Anzeigen einer Anforderung und nach Bedarf das Abfragen von Qualifikationen zu den Produkten'},
+  'MultipleChoice': {title: 'Multiple Choice', tooltip: 'Eine Frage mit mehreren Antwortmöglichkeiten'},
+  'Slider': {title: 'Slider', tooltip: 'Eine Frage mit einer Skala als Antwortmöglichkeit'},
+  'Textfield': {title: 'Info', tooltip: 'Ein Freitextfeld für zusätzliche Informationen '},
+  'Note': {title: 'Notizen', tooltip: 'Ein Freitextfeld für Notizen des Studenten'},
+};
+
 const { isDragging } = toRefs(collect);
 const isHovered = ref(false);
+
+const translatedTitle = computed(() => {
+  return componentsTranslationMap[props.title]?.title || props.title;
+});
+
+const translatedTooltip = computed(() => {
+  return componentsTranslationMap[props.title]?.tooltip || '';
+});
 
 function onHover(state: boolean) {
   isHovered.value = state;
@@ -39,10 +56,13 @@ function onHover(state: boolean) {
             v-element-hover="onHover"
             :variant="isHovered ? 'outlined' : 'elevated'"
             border
-            v-bind="props"
             ripple
             density="compact"
+            :title="translatedTitle"
         >
+          <v-tooltip location="bottom" activator="parent">
+            {{ translatedTooltip }}
+          </v-tooltip>
         </v-card>
   </div>
 </template>
