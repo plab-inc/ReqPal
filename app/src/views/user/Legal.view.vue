681b138d308a6c2822fe74e70bb7cf9eb00f18d1
Fabian Teufel <fabianteufel@me.com>
2023-11-12 19:20:34 +0100

    Add legal notice

---
+++ app/src/views/user/Legal.view.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row justify="space-between" align="center" class="mb-1">
+    <v-col cols="auto" class="text-h4">
+      Nutzungsbedingungen
+    </v-col>
+  </v-row>
+  <v-divider></v-divider>
+  <v-container>
+    <p>Herzlich Willkommen bei Projektarbeit2023. Diese rechtlichen Hinweise gelten für alle Nutzer, die sich auf unserer Website anmelden.</p>
+    <p>Durch die Registrierung auf unserer Website verpflichten Sie sich, wahre, aktuelle und vollständige Informationen über sich selbst anzugeben. Die Anmeldung erfolgt durch Ausfüllen unseres Anmeldeformulars und Bestätigung Ihrer E-Mail-Adresse.</p>
+    <p>Ihre persönlichen Daten werden gemäß der DSGVO und anderen relevanten Datenschutzgesetzen behandelt. Wir sammeln Daten, um unseren Service zu verbessern und personalisieren. Sie haben das Recht, auf Ihre Daten zuzugreifen, sie zu korrigieren oder ihre Löschung zu verlangen.</p>
+    <p>Die Nutzung unserer Website ist an Bedingungen geknüpft. Verboten ist die Verbreitung illegaler Inhalte, Belästigung anderer Nutzer und jede Aktivität, die gegen geltendes Recht verstößt.</p>
+    <p>Alle Inhalte auf unserer Website sind unser Eigentum oder das von Dritten, mit denen wir zusammenarbeiten. Inhalte, die von Nutzern erstellt werden, unterliegen bestimmten Regeln, die in unseren Nutzungsbedingungen festgelegt sind.</p>
+    <p>Unsere Haftung für die Nutzung unserer Website ist auf Vorsatz und grobe Fahrlässigkeit beschränkt.</p>
+    <p>Wir behalten uns das Recht vor, diesen rechtlichen Hinweis zu ändern. Änderungen werden den Nutzern rechtzeitig mitgeteilt.</p>
+    <p>Für Streitigkeiten, die aus der Nutzung unserer Website entstehen, gilt das Recht der Bundesrepublik Deutschland. Gerichtsstand ist Dortmund, NRW.</p>
+    <p>Für rechtliche Fragen oder Anliegen bezüglich unserer Website, kontaktieren Sie uns bitte unter support@lethalgoose.com.</p>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
