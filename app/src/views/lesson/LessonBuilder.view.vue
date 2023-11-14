927677649712573b78232fd994c83d23db7401d6
Laura <seifert.laura@gmx.de>
2023-11-14 18:25:01 +0100

    move toggle button to top of view

--- app/src/views/lesson/LessonBuilder.view.vue
+++ app/src/views/lesson/LessonBuilder.view.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-
 import LessonBuilder from "@/components/lesson/lessonBuilder/LessonBuilder.component.vue";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import {DndProvider} from "vue3-dnd";
 import {useLessonFormStore} from "@/stores/lessonForm.store.ts";
 
 const lessonFormStore = useLessonFormStore();
-
+const showToolTips = ref<boolean>(true);
 </script>
 
 <template>
@@ -14,11 +13,27 @@ const lessonFormStore = useLessonFormStore();
     <v-col cols="auto" class="text-h4">
       {{ lessonFormStore.isDirty ? 'Lektion bearbeiten' : 'Neue Lektion erstellen' }}
     </v-col>
+    <v-col cols="auto">
+      <v-btn-toggle
+          elevation="3"
+          v-model="showToolTips"
+          variant="outlined"
+          rounded
+          divided
+          color="warning"
+      >
+        <v-btn
+            :value="true"
+        >
+          Modulbeschreibungen anzeigen
+        </v-btn>
+      </v-btn-toggle>
+    </v-col>
   </v-row>
   <v-divider></v-divider>
   <DndProvider :backend="HTML5Backend">
     <v-container>
-      <LessonBuilder/>
+      <LessonBuilder :show-tool-tip="showToolTips ? showToolTips : false"/>
     </v-container>
   </DndProvider>
 </template>
