3f262a57aaf620d7cb522440df3a33edfe849095
Fabian Teufel <fabianteufel@me.com>
2023-11-26 22:48:07 +0100

    #119 Catalog upload function refactor

--- supabase/database/functions/catalog.sql
+++ supabase/database/functions/catalog.sql
@@ -5,16 +5,14 @@ create or replace function upload_catalog_to_database(p_catalog_name text, p_pro
 as
 $$
 DECLARE
-    v_catalog_id        integer;
-    v_product_ids       int[];
-    v_product_ids_index integer DEFAULT 1;
-    v_requirement       jsonb;
-    v_requirement_id    integer;
+    v_catalog_id       integer;
+    v_requirement      jsonb;
+    v_requirement_id   integer;
     v_product           text;
     v_details           jsonb;
 BEGIN
-
-    INSERT INTO catalogs (catalog_name, user_id) VALUES (p_catalog_name, auth.uid()) RETURNING catalog_id INTO v_catalog_id;
+    INSERT INTO catalogs (catalog_name, user_id) VALUES (p_catalog_name, auth.uid())
+    RETURNING catalog_id INTO v_catalog_id;
 
     WITH ins AS (
         INSERT INTO products (product_name, product_url)
@@ -23,12 +21,8 @@ BEGIN
             ON CONFLICT (product_name) DO UPDATE
                 SET product_name = EXCLUDED.product_name, product_url = EXCLUDED.product_url
             RETURNING product_id)
-    SELECT ARRAY_AGG(product_id)
-    INTO v_product_ids
-    FROM ins;
-
     INSERT INTO product_catalogs (product_id, catalog_id)
-    SELECT unnest(v_product_ids), v_catalog_id;
+    SELECT product_id, v_catalog_id FROM ins;
 
     FOR v_requirement IN SELECT * FROM jsonb_array_elements(p_requirements)
         LOOP
@@ -36,19 +30,13 @@ BEGIN
             VALUES (v_catalog_id, v_requirement ->> 'reqId', v_requirement ->> 'title', v_requirement ->> 'description')
             RETURNING requirement_id INTO v_requirement_id;
 
-            v_product_ids_index := 1;
-
             FOR v_product, v_details IN SELECT * FROM jsonb_each(v_requirement -> 'productDetails')
                 LOOP
                     INSERT INTO product_requirements (product_id, requirement_id, qualification, comment)
-                    VALUES (v_product_ids[v_product_ids_index],
-                            v_requirement_id,
-                            v_details ->> 'qualification',
-                            v_details ->> 'comment');
-                    v_product_ids_index := v_product_ids_index + 1;
+                    SELECT product_id, v_requirement_id, v_details ->> 'qualification', v_details ->> 'comment'
+                    FROM products
+                    WHERE product_name = UPPER(REPLACE(v_product, ' ', ''));
                 END LOOP;
-
         END LOOP;
-
 END;
 $$;
