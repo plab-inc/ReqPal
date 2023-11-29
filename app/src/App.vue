c15993049fe0d0502b8a231fb71b80165fbf773d
Laura <seifert.laura@gmx.de>
2023-11-29 15:24:56 +0100

    add console logs for testing

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
-import { supabase } from "@/plugins/supabase";
-import { useAuthStore } from "@/stores/auth.store";
-import { useThemeStore } from '@/stores/theme.store';
-import { useTheme } from "vuetify";
-import { unhandledRejectionHandler, globalErrorHandler } from "@/errors/handler.errors.ts";
+import {supabase} from "@/plugins/supabase";
+import {useAuthStore} from "@/stores/auth.store";
+import {useThemeStore} from '@/stores/theme.store';
+import {useTheme} from "vuetify";
+import {unhandledRejectionHandler, globalErrorHandler} from "@/errors/handler.errors.ts";
 
 const themeStore = useThemeStore();
 const theme = useTheme();
 
-onMounted(() => {
-  themeStore.syncWithBrowserSettings();
-  applyTheme(themeStore.currentTheme);
+themeStore.syncWithBrowserSettings();
+applyTheme(themeStore.currentTheme);
 
-  window.addEventListener('unhandledrejection', unhandledRejectionHandler);
-  window.addEventListener('error', globalErrorHandler);
-
-});
+window.addEventListener('unhandledrejection', unhandledRejectionHandler);
+window.addEventListener('error', globalErrorHandler);
 
 watch(() => themeStore.currentTheme, (newTheme) => {
   applyTheme(newTheme);
@@ -27,7 +24,7 @@ supabase.auth.onAuthStateChange((event, session) => {
   }
 });
 
-function applyTheme(selectedTheme: string){
+function applyTheme(selectedTheme: string) {
   theme.global.name.value = selectedTheme;
 }
 
