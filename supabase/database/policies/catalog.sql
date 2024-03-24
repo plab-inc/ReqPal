e4539ed7dd2a7c41d6b5df00124d5e2d29264282
Laura <laura.seifert002@stud.fh-dortmund.de>
2024-03-24 15:47:23 +0100

    update catalog policies

--- supabase/database/policies/catalog.sql
+++ supabase/database/policies/catalog.sql
@@ -24,7 +24,6 @@ CREATE POLICY "policy_catalogs_select"
             get_teacher_uuid(auth.uid() ) = user_id
     );
 
-
 DROP POLICY IF EXISTS "policy_catalogs_example" ON public.catalogs;
 CREATE POLICY "policy_catalogs_example"
     ON public.catalogs
@@ -34,18 +33,60 @@ CREATE POLICY "policy_catalogs_example"
         (SELECT example) = true
     );
 
+DROP POLICY IF EXISTS "policy_requirements_select" ON public.requirements;
+CREATE POLICY "policy_requirements_select"
+    ON public.requirements
+    FOR SELECT
+    TO authenticated
+    USING (
+    EXISTS(SELECT 1
+           FROM catalogs
+           WHERE catalogs.catalog_id = catalog_id)
+        AND (
+        EXISTS(SELECT 1
+               FROM catalogs
+               WHERE catalogs.catalog_id = catalog_id
+                 and catalogs.example = true)
+            OR
+        EXISTS(SELECT 1
+               FROM catalogs
+               WHERE catalogs.catalog_id = catalog_id
+                 and (get_teacher_uuid(auth.uid()) = catalogs.user_id))
+        )
+    );
+
 DROP POLICY IF EXISTS "policy_requirements" ON public.requirements;
 CREATE POLICY "policy_requirements"
     ON public.requirements
     FOR ALL
     TO authenticated
     USING (
-    EXISTS(
-        SELECT 1
-        FROM catalogs
-        WHERE catalogs.catalog_id = catalog_id
-    )
+    EXISTS(SELECT 1
+           FROM catalogs
+           WHERE catalogs.catalog_id = catalog_id)
+        AND
+    (
+        (SELECT check_user_role(auth.uid(), 'moderator')) = true
+            OR (
+            (SELECT check_user_role(auth.uid(), 'teacher'))
+                AND
+            EXISTS(SELECT 1
+                   FROM catalogs
+                   WHERE catalogs.catalog_id = catalog_id
+                     and auth.uid() = catalogs.user_id)
+            ))
     );
+
+DROP POLICY IF EXISTS "policy_product_requirements_select" ON public.product_requirements;
+CREATE POLICY "policy_product_requirements_select"
+    ON public.product_requirements
+    FOR SELECT
+    TO authenticated
+    USING (
+    EXISTS(SELECT 1
+           FROM requirements
+           WHERE requirements.requirement_id = product_requirements.requirement_id));
+
 DROP POLICY IF EXISTS "policy_product_requirements" ON public.product_requirements;
 CREATE POLICY "policy_product_requirements"
     ON public.product_requirements
@@ -79,15 +120,32 @@ CREATE POLICY "policy_products_update"
     FOR UPDATE
     USING ((select check_user_role(auth.uid(), 'teacher')) = true);
 
-DROP POLICY IF EXISTS "policy_product_catalog_insert" ON public.product_catalogs;
-CREATE POLICY "policy_product_catalog_insert"
+DROP POLICY IF EXISTS "policy_product_catalogs_insert" ON public.product_catalogs;
+CREATE POLICY "policy_product_catalogs_insert"
     ON public.product_catalogs
     FOR INSERT
     TO authenticated
-    WITH CHECK ((select check_user_role(auth.uid(), 'teacher')) = true);
+    WITH CHECK (
+    (select check_user_role(auth.uid(), 'teacher') = true)
+    );
 
-DROP POLICY IF EXISTS "policy_product_catalog_select" ON public.product_catalogs;
-CREATE POLICY "policy_product_catalog_select"
+DROP POLICY IF EXISTS "policy_product_catalogs_delete" ON public.product_catalogs;
+CREATE POLICY "policy_product_catalogs_delete"
+    ON public.product_catalogs
+    FOR DELETE
+    TO authenticated
+    USING (
+    ((select check_user_role(auth.uid(), 'teacher')) = true)
+        AND
+    EXISTS(
+        (SELECT 1
+         FROM catalogs
+                  JOIN product_catalogs ON catalogs.catalog_id = product_catalogs.catalog_id
+         WHERE catalogs.user_id = auth.uid())
+    ));
+
+DROP POLICY IF EXISTS "policy_product_catalogs_select" ON public.product_catalogs;
+CREATE POLICY "policy_product_catalogs_select"
     ON public.product_catalogs
     FOR SELECT
     TO authenticated
