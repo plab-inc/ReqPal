9783c56c421d5b7ee75c63f12135ea8e8ebe91b5
Fabian Teufel <27745262+TheLethalGoose@users.noreply.github.com>
2024-06-01 22:51:04 +0200

    Supabase App_Guard

--- bpmn-server/src/app.module.ts
+++ bpmn-server/src/app.module.ts
@@ -2,11 +2,21 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BPMNModule } from "./bpmn/bpmn.module";
+import { ConfigModule } from '@nestjs/config';
+import { SupabaseGuard, SupabaseModule } from "./common/supabase/index";
+import { APP_GUARD } from "@nestjs/core";
+import { PassportModule } from '@nestjs/passport';
 
 
 @Module({
-  imports: [BPMNModule],
+  imports: [BPMNModule, ConfigModule.forRoot(), PassportModule, SupabaseModule ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+      AppService,
+      {
+        provide: APP_GUARD,
+        useClass: SupabaseGuard,
+      }
+    ],
 })
 export class AppModule {}
