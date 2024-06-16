7bf5dcfb7cd89315d1cb4092e6200279899def79
Fabian <27745262+TheLethalGoose@users.noreply.github.com>
2024-06-16 15:22:50 +0200

    Basic rate limit without ip blocking

--- bpmn-server/src/app.module.ts
+++ bpmn-server/src/app.module.ts
@@ -2,21 +2,36 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BPMNModule } from "./bpmn/bpmn.module";
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from "@nestjs/config";
 import { SupabaseGuard, SupabaseModule } from "./common/supabase/index";
 import { APP_GUARD } from "@nestjs/core";
-import { PassportModule } from '@nestjs/passport';
+import { PassportModule } from "@nestjs/passport";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 
 
 @Module({
-  imports: [BPMNModule, ConfigModule.forRoot(), PassportModule, SupabaseModule ],
+  imports: [
+    BPMNModule,
+    ConfigModule.forRoot(),
+    PassportModule,
+    SupabaseModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10
+    }])
+  ],
   controllers: [AppController],
   providers: [
-      AppService,
-      {
-        provide: APP_GUARD,
-        useClass: SupabaseGuard,
-      }
-    ],
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: SupabaseGuard
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
