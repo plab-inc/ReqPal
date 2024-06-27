22c0ad72e11a2ebaa3971cfab918d46c03166501
Fabian <27745262+TheLethalGoose@users.noreply.github.com>
2024-06-27 20:47:57 +0200

    GamificationService and delegate

---
+++ bpmn-server/src/main/java/inc/plab/bpmn/service/GamificationService.java
@@ -0,0 +1,11 @@
+package inc.plab.bpmn.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GamificationService {
+
+    public void hello() {
+        System.out.println("Hello from GamificationService");
+    }
+}
