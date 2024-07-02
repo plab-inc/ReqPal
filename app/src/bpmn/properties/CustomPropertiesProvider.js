4f8a81af38dde9b0bdd4694e03005072e56a9bc4
Fabian Teufel <27745262+TheLethalGoose@users.noreply.github.com>
2024-07-03 01:00:51 +0200

    Overwrite general group

--- app/src/bpmn/properties/CustomPropertiesProvider.js
+++ app/src/bpmn/properties/CustomPropertiesProvider.js
@@ -1,34 +1,31 @@
 import { ConditionGroup } from "./parts/ConditionGroup.js";
 import { UserTaskGroup } from "./parts/UserTaskLesson.js";
+import { GeneralGroup } from "@/bpmn/properties/parts/GeneralGroup.js";
 
 export default function CustomPropertiesProvider(propertiesPanel, translate) {
 
   this.getGroups = function(element) {
     return function(groups) {
 
-      const deleteCamundaPlatformGroups = (groups) => {
-        return groups.filter(group => !(group.id.startsWith("CamundaPlatform__")));
+      const deleteGroupsToReplace = (groups) => {
+        return groups.filter(group => !(group.id.startsWith("CamundaPlatform__") || group.id === "general"));
       };
 
-      const conditionGroup= ConditionGroup(element, translate);
-      const userTaskLessonGroup = UserTaskGroup(element, translate)
+      const conditionGroup = ConditionGroup(element, translate);
+      const userTaskLessonGroup = UserTaskGroup(element, translate);
+      const generalGroup = GeneralGroup(element, translate);
 
-      let filteredGroups = deleteCamundaPlatformGroups(groups);
+      let filteredGroups = deleteGroupsToReplace(groups);
 
-      if(conditionGroup !== null){
-        filteredGroups.push(conditionGroup);
-      }
-
-      if(userTaskLessonGroup !== null){
-        filteredGroups.push(userTaskLessonGroup);
-      }
+      if (conditionGroup) filteredGroups.push(conditionGroup);
+      if (generalGroup) filteredGroups.unshift(generalGroup);
+      if (userTaskLessonGroup) filteredGroups.push(userTaskLessonGroup);
 
       return filteredGroups;
-
     };
   };
 
   propertiesPanel.registerProvider(500, this);
 }
 
-CustomPropertiesProvider.$inject = [ 'propertiesPanel', 'translate' ];
+CustomPropertiesProvider.$inject = ['propertiesPanel', 'translate'];
