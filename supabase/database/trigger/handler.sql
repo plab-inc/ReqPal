7b79095284d4c64e8047efb7a0d972af0ae719c9
Laura Seifert <92450387+Magichoco@users.noreply.github.com>
2024-07-08 13:35:59 +0200

    update handle_new_user to assign 15 xp to students for a new registration

--- supabase/database/trigger/handler.sql
+++ supabase/database/trigger/handler.sql
@@ -7,8 +7,9 @@ create or replace function handle_new_user() returns trigger
 as
 $$
 DECLARE
-    user_role text;
+    user_role  text;
     teacher_id uuid := NULL;
+    xp INTEGER := 15;
 begin
     user_role := new.raw_user_meta_data ->> 'role';
 
@@ -28,13 +29,21 @@ begin
     perform set_claim(new.id, 'userroles', jsonb_build_array(user_role));
     perform update_user_permissions(new.id);
 
+    IF user_role = 'student' THEN
+        INSERT INTO user_statistics (user_id, total_xp)
+        VALUES (new.id, xp);
+
+        INSERT INTO xp_activity_logs (user_id, action, received_xp)
+        VALUES (new.id, 'Registrierung', xp);
+    end if;
+
     return new;
 end;
 $$;
 
-drop trigger if exists handle_new_user_trigger on auth.users;
-create trigger handle_new_user_trigger
+drop trigger if exists create_profile_trigger on auth.users;
+create trigger create_profile_trigger
     after insert
     on auth.users
     for each row
-execute procedure public.handle_new_user();
+    execute procedure public.handle_new_user();
