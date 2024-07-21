7c490897e8993ed79ffd9725f7dba4df6531fcd3
Laura Seifert <92450387+Magichoco@users.noreply.github.com>
2024-07-21 09:33:39 +0200

    set correct id for where clause

--- supabase/database/trigger/reqpalAchievements.sql
+++ supabase/database/trigger/reqpalAchievements.sql
@@ -10,20 +10,24 @@ DROP TRIGGER IF EXISTS handle_xp_achievement_trigger_update ON user_statistics;
 DROP FUNCTION IF EXISTS update_xp_achievements();
 
 CREATE OR REPLACE FUNCTION update_xp_achievements()
-    RETURNS TRIGGER LANGUAGE plpgsql AS $$
+    RETURNS TRIGGER
+    LANGUAGE plpgsql AS
+$$
 DECLARE
     achievement_ids UUID[];
-    achievement_id UUID;
-    new_level INT;
-    max_level INT;
+    achievement_id  UUID;
+    new_level       INT;
+    max_level       INT;
 BEGIN
-    SELECT array_agg(id) INTO achievement_ids
+    SELECT array_agg(id)
+    INTO achievement_ids
     FROM reqpal_achievements
     WHERE target_field = 'total_xp';
 
     FOREACH achievement_id IN ARRAY achievement_ids
         LOOP
-            SELECT level, threshold INTO new_level
+            SELECT level, threshold
+            INTO new_level
             FROM reqpal_achievement_levels
             WHERE reqpal_achievement_id = achievement_id
               AND NEW.total_xp >= threshold
@@ -31,24 +35,22 @@ BEGIN
             LIMIT 1;
 
             IF FOUND THEN
-                IF NOT EXISTS (
-                    SELECT 1
-                    FROM user_reqpal_achievements
-                    WHERE user_id = NEW.user_id
-                      AND reqpal_achievement_id = achievement_id
-                      AND level = new_level
-                ) THEN
-                    SELECT MAX(level) INTO max_level
+                IF NOT EXISTS (SELECT 1
+                               FROM user_reqpal_achievements
+                               WHERE user_id = NEW.user_id
+                                 AND reqpal_achievement_id = achievement_id
+                                 AND level = new_level) THEN
+                    SELECT MAX(level)
+                    INTO max_level
                     FROM reqpal_achievement_levels
-                    WHERE reqpal_achievement_id = reqpal_achievement_id;
+                    WHERE reqpal_achievement_id = achievement_id;
 
                     INSERT INTO user_reqpal_achievements (user_id, reqpal_achievement_id, created_at, level, max)
                     VALUES (NEW.user_id, achievement_id, NOW(), new_level, (new_level >= max_level))
                     ON CONFLICT (user_id, reqpal_achievement_id)
-                        DO UPDATE SET
-                                      level = EXCLUDED.level,
+                        DO UPDATE SET level      = EXCLUDED.level,
                                       created_at = EXCLUDED.created_at,
-                                      max = EXCLUDED.level >= max_level;
+                                      max        = (EXCLUDED.level >= max_level);
                 END IF;
             END IF;
         END LOOP;
@@ -67,6 +69,7 @@ CREATE TRIGGER handle_xp_achievement_trigger_update
     ON user_statistics
     FOR EACH ROW
 EXECUTE FUNCTION update_xp_achievements();
+
 
 --------------------------------------------
 -- update max flag in case new levels are added to existing achievements and already gained user levels
