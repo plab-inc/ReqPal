63ddcc99b424e76036373270ec11c122eba2c26e
Laura <laura.seifert002@stud.fh-dortmund.de>
2024-08-05 22:21:04 +0200

    add tooltip to achievement chips

---
+++ app/src/components/achievement/AchievementChip.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-tooltip location="top center" :text="achievement.description">
+    <template v-slot:activator="{ props }">
+      <v-chip :class="chipClass" color="success" v-bind="props" @click="router.push({name: 'Profil'})">
+        <v-icon class="mr-2">
+          <v-img :src="getAchievementImageUrl(achievement.image)"
+                 :alt="'ReqPal-Achievement Level Image: '+achievement.image"></v-img>
+        </v-icon>
+        {{ achievement.title }}
+      </v-chip>
+    </template>
+  </v-tooltip>
+</template>
+
+<script setup lang="ts">
+import {Achievement} from "@/types/achievement.ts";
+import {getAchievementImageUrl} from "@/utils/achievementImage.ts";
+import router from "@/router";
+
+interface Props {
+  achievement: Achievement;
+  chipClass?: string;
+}
+
+defineProps<Props>();
+</script>
+
