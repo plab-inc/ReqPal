c4fab6fe13c0d12447baea607c6db5698fcf8c2c
Laura <seifert.laura@gmx.de>
2024-08-14 20:33:47 +0200

    delete database folder and evaluation edge function

--- supabase/database/pgCronJobs/activityLogs.sql
+++
@@ -1,8 +0,0 @@
--- Author: Laura
-
--- delete logs every day at 3am older than a day
-SELECT cron.schedule(
-               'daily-xp-logs-cleanup',
-               '0 3 * * *',
-               $$ DELETE FROM xp_activity_logs WHERE created_at < NOW() - INTERVAL '1 day' $$
-       );
--- supabase/database/pgCronJobs/cronJobs.sql
+++
@@ -1,5 +0,0 @@
--- Author: Laura
-
--- Delete old cron.job_run_details records of the current user every day at noon
-SELECT cron.schedule('delete-job-run-details', '0 12 * * *',
-                     $$DELETE FROM cron.job_run_details WHERE end_time < now() - interval '7 days'$$);
