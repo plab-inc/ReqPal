50f7eae7d5561742635497ce1eb4230ef364d2a7
Fabian Teufel <27745262+TheLethalGoose@users.noreply.github.com>
2024-09-02 16:25:04 +0200

    BPMN Linter

--- app/src/bpmn/modeler/customContextPad.ts
+++ app/src/bpmn/modeler/customContextPad.ts
@@ -33,6 +33,14 @@ class CustomContextPad implements ContextPadProvider {
   getContextPadEntries(element: Element) {
     return (entries: ContextPadEntries): ContextPadEntries => {
       this.removeUnusedEntries(entries);
+
+      if (element.type === "bpmn:EndEvent") {
+        return {
+          "delete": entries["delete"],
+          "set-color": entries["set-color"]
+        };
+      }
+
       const taskEntries = this.createTaskEntries();
 
       if (element.type === "label" || element.type === "bpmn:SequenceFlow" || element.type === "bpmn:Group") {
@@ -51,11 +59,12 @@ class CustomContextPad implements ContextPadProvider {
     };
   }
 
-
   private removeUnusedEntries(entries: ContextPadEntries) {
     const keysToRemove = [
       'append.intermediate-event',
-      'append.append-task'
+      "append.append-task",
+      "append.gateway",
+      "append.end-event"
     ];
 
     keysToRemove.forEach(key => delete entries[key]);
@@ -90,10 +99,38 @@ class CustomContextPad implements ContextPadProvider {
       }
     };
 
+    const appendExclusiveGatewayStart = (event: any, element: ContextPadTarget) => {
+      const shape = this.createExclusiveGatewayShape();
+      this.create.start(event, shape, element);
+    };
+
+    const appendExclusiveGateway = (event: any, element: ContextPadTarget) => {
+      if (this.autoPlace) {
+        const shape = this.createExclusiveGatewayShape();
+        this.autoPlace.append(element, shape);
+      } else {
+        appendExclusiveGatewayStart(event, element);
+      }
+    };
+
+    const appendEndEventStart = (event: any, element: ContextPadTarget) => {
+      const shape = this.createEndEventShape();
+      this.create.start(event, shape, element);
+    };
+
+    const appendEndEvent = (event: any, element: ContextPadTarget) => {
+      if (this.autoPlace) {
+        const shape = this.createEndEventShape();
+        this.autoPlace.append(element, shape);
+      } else {
+        appendEndEventStart(event, element);
+      }
+    };
+
     return {
       'append.user-task': {
         className: 'bpmn-icon-user-task',
-        title: this.translate('Append Lesson Task'),
+        title: this.translate("Append User Task"),
         action: {
           dragstart: appendUserTaskStart,
           click: appendUserTask
@@ -101,10 +138,26 @@ class CustomContextPad implements ContextPadProvider {
       },
       'append.service-task': {
         className: 'bpmn-icon-service-task',
-        title: this.translate('Append Gamification Service'),
+        title: this.translate("Append Service Task"),
         action: {
           dragstart: appendServiceTaskStart,
           click: appendServiceTask
+        }
+      },
+      "append.exclusive-gateway": {
+        className: "bpmn-icon-gateway-xor",
+        title: this.translate("Append Exclusive Gateway"),
+        action: {
+          dragstart: appendExclusiveGatewayStart,
+          click: appendExclusiveGateway
+        }
+      },
+      "append.end-event": {
+        className: "bpmn-icon-end-event-none",
+        title: this.translate("Append End Event"),
+        action: {
+          dragstart: appendEndEventStart,
+          click: appendEndEvent
         }
       }
     };
@@ -128,6 +181,24 @@ class CustomContextPad implements ContextPadProvider {
     });
   }
 
+  private createExclusiveGatewayShape() {
+    const businessObject = this.bpmnFactory.create("bpmn:ExclusiveGateway");
+
+    return this.elementFactory.createShape({
+      type: "bpmn:ExclusiveGateway",
+      businessObject: businessObject
+    });
+  }
+
+  private createEndEventShape() {
+    const businessObject = this.bpmnFactory.create("bpmn:EndEvent");
+    businessObject.name = "Ende";
+
+    return this.elementFactory.createShape({
+      type: "bpmn:EndEvent",
+      businessObject: businessObject
+    });
+  }
 }
 
 CustomContextPad.$inject = ['config','contextPad', 'create', 'bpmnFactory', 'elementFactory', 'translate', 'injector'];
